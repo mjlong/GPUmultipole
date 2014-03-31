dc8045e6aad3210fd4451216055d5931004e8497
Jilang Miao 缪佶朗 <jlmiao@mit.edu>
2014-03-31 09:21:49 -0400

    init multipole calculation

---
+++ multipole.cpp
@@ -0,0 +1,1 @@
+#include "multipole.h"
---
+++ multipole.h
@@ -0,0 +1,18 @@
+#ifndef __MULTIPOLE_H__
+#define __MULTIPOLE_H__
+#include "isotope.h"
+
+#define MP_EA 1
+#define MP_RT 2
+#define MP_RA 3
+#define MP_RF 4
+
+class multipole{
+ private:
+
+ public:
+
+
+}
+
+#endif
