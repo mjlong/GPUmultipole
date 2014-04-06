1fa4f05684668251cc65392c4b529db5c3a1bbba
Jilang Miao 缪佶朗 <jlmiao@mit.edu>
2014-04-06 16:40:38 -0400

    cleanup

--- h5_rdwt.cpp
+++ h5_rdwt.cpp
@@ -5,21 +5,24 @@
 #include <iostream>
 #define FILE "092238.h5"
 
-#include "CPUComplex.h"
+typedef struct{
+  double complex[2];
+}tuple;
+
 
 int main() {
+  tuple *complex;
+  hid_t file_id, dataset_id, complex_id, complex_array_id, space_id;  /* identifiers */
+  hsize_t dims[2], datasize_1d[1];
+  herr_t status;
 
-  hid_t       file_id, dataset_id, complex_id, complex_array_id;  /* identifiers */
-  hsize_t datasize_1d[1];
-  herr_t      status;
-
-  int         i, j, k, rank,  
+  int i, j, k, rank, ndims,
     fitorder[1], dset_data[1], length[1];
   int * l_value;
   double *fit;
-  double *mpdatd;
-  //  complex<double> *mpdata;
-  CComplex *mpdata;
+  double *mpole;
+  double **mpoles;
+
   file_id = H5Fopen(FILE, H5F_ACC_RDONLY, H5P_DEFAULT);
 
   dataset_id = H5Dopen(file_id, "/isotop/fitorder", H5P_DEFAULT);
@@ -46,33 +49,22 @@ int main() {
   free(fit);
   status = H5Dclose(dataset_id);
 
+  dataset_id = H5Dopen(file_id, "/isotop/mpdata", H5P_DEFAULT);
+  
+  space_id = H5Dget_space(dataset_id);
+  ndims = H5Sget_simple_extent_dims(space_id, dims, NULL);
+  printf("number of dims:%d, length of dim1:%d,of dim2:%d\n",(int)ndims,(int)dims[0],(int)dims[1]);
+  //  dims[0] = 4;
+  //  dims[1] = length[0];
+  rank = 1; 
+  datasize_1d[0]=2;
+  complex_array_id = H5Tarray_create(H5T_NATIVE_DOUBLE, rank, datasize_1d);
+  complex_id = H5Tcreate(H5T_COMPOUND, sizeof(tuple));
+  status = H5Tinsert(complex_id, "point", HOFFSET(tuple, complex), complex_array_id);
+  complex = (tuple*)malloc(dims[0]*dims[1]*sizeof(tuple));
+  status = H5Dread(dataset_id, complex_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, complex);
+  printf("%g+%gi\n",complex[4].complex[0],complex[4].complex[1]);
   
   status = H5Fclose(file_id);
 }
 
-  /*  char real[]="real";
-  char imag[]="imaginary";
-  H5Tinsert (complex_id, real, sizeof(double), H5T_NATIVE_DOUBLE);
-  H5Tinsert (complex_id, imag, sizeof(double), H5T_NATIVE_DOUBLE);  */
-
-/*
-  //complex_id = H5Tcreate(H5T_COMPOUND,2*sizeof(double));// sizeof(CComplex));//
-  
-  mpdatd = (double*)malloc(2*4*length[0]*sizeof(double));
-  dataset_id = H5Dopen(file_id, "/isotop/mpdata",H5P_DEFAULT);
-  status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, mpdatd);
-  free(mpdatd);
-
-  mpdata = (CComplex*)malloc(4*length[0]*2*sizeof(double));
-  rank = 1; 
-  datasize_1d[0]=2;
-  complex_array_id = H5Tarray_create(H5T_NATIVE_DOUBLE, rank, datasize_1d);
-  dataset_id = H5Dopen(file_id, "/isotop/mpdata",H5P_DEFAULT);
-  status = H5Dread(dataset_id, complex_array_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, mpdata);
-  //char str[]="point";
-  //status = H5Tinsert(complex_id, str , 2*sizeof(double),complex_array_id);
-  //status = H5Tinsert(complex_array_id, str , 2*sizeof(double),complex_id);
-  free(mpdata);
-
-
- */
