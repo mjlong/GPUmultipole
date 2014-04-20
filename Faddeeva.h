a27d4f90be0606a6ea2f00aa9205e282650f99bf
Jilang Miao 缪佶朗 <jlmiao@mit.edu>
2014-04-20 11:34:51 -0400

    Faddeeva debugged from parallel/cuda/ and complex/ repo

--- Faddeeva.h
+++ Faddeeva.h
@@ -27,35 +27,35 @@
 #ifndef FADDEEVA_HH
 #define FADDEEVA_HH 
 
-#include "CPUComplex.h"
+#include "CComplex.h"
 
 namespace Faddeeva {
 
 // compute w(z) = exp(-z^2) erfc(-iz) [ Faddeeva / scaled complex error func ]
-extern CComplex w(CComplex z,double relerr=0);
-extern double w_im(double x); // special-case code for Im[w(x)] of real x
+extern __device__ CComplex w(CComplex z,double relerr=0);
+extern __device__ double w_im(double x); // special-case code for Im[w(x)] of real x
 
 // Various functions that we can compute with the help of w(z)
 
 // compute erfcx(z) = exp(z^2) erfc(z)
-extern CComplex erfcx(CComplex z, double relerr=0);
-extern double erfcx(double x); // special case for real x
+extern __device__ CComplex erfcx(CComplex z, double relerr=0);
+extern __device__ double erfcx(double x); // special case for real x
 
 // compute erf(z), the error function of complex arguments
-extern CComplex erf(CComplex z, double relerr=0);
-extern double erf(double x); // special case for real x
+extern __device__ CComplex erf(CComplex z, double relerr=0);
+extern __device__ double erf(double x); // special case for real x
 
 // compute erfi(z) = -i erf(iz), the imaginary error function
-extern CComplex erfi(CComplex z, double relerr=0);
-extern double erfi(double x); // special case for real x
+extern __device__ CComplex erfi(CComplex z, double relerr=0);
+extern __device__ double erfi(double x); // special case for real x
 
 // compute erfc(z) = 1 - erf(z), the complementary error function
-extern CComplex erfc(CComplex z, double relerr=0);
-extern double erfc(double x); // special case for real x
+extern __device__ CComplex erfc(CComplex z, double relerr=0);
+extern __device__ double erfc(double x); // special case for real x
 
 // compute Dawson(z) = sqrt(pi)/2  *  exp(-z^2) * erfi(z)
-extern CComplex Dawson(CComplex z, double relerr=0);
-extern double Dawson(double x); // special case for real x
+extern __device__  CComplex Dawson(CComplex z, double relerr=0);
+extern __device__ double Dawson(double x); // special case for real x
 
 } // namespace Faddeeva
 
