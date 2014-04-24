c7c1eb3d8e64ddca8e88853eccb77956d83ebf39
jlmiao@nsecluster <jlmiao@mit.edu>
2014-04-24 17:53:09 -0400

    launch a pseudo kernal

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "h5_rdwt.h"
 #include "multipole.h"
 #define NUM 10
+
+__global__ void evaluate(multipole pole, double energy, double sqrKT, double *sigT, double *sigA, double *sigF){
+  U238.xs_eval_fast(energy,sqrt(KB*T),sigT,sigA,sigF);
+}
+
+
 int main(){
+  size_t size = 2*sizeof(double);
   char h5filename[] = "092238.h5";
   double sigT, sigA, sigF;
-  double sibT, sibA, sibF;
+  double *dev_sigT, *dev_sigA, *dev_sigF;
+  cudaMalloc((void**)&dev_sigT, size);
+  cudaMalloc((void**)&dev_sigA, size);
+  cudaMalloc((void**)&dev_sigF, size);
   double e0=10.0,e1=100000.0,rnd, energy;
   double T = 900.0; 
   multipole U238;
@@ -17,12 +27,16 @@ int main(){
     //    rnd = rand()/(double)RAND_MAX;
     //    energy = e0 + rnd*(e1-e0);
     energy = (i+1.0)*1.63;
-    U238.xs_eval_fast(energy,sqrt(KB*T),sigT,sigA,sigF);
-    U238.xs_eval_fast(energy           ,sibT,sibA,sibF);
-    fprintf(file, "%8.4f %8.5e %8.5e %8.5e %8.5e %8.5e %8.5e\n", 
-	    energy, sigT, sibT, sigA, sibA, sigF, sibF);
+    evaluate<<<1,1>>>(energy, sqrt(KB*T), dev_sigT, dev_sigA, dev_sigF);
+    cudaMemcpy(&sigT, dev_sigT, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(&sigA, dev_sigA, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(&sigF, dev_sigF, size, cudaMemcpyDeviceToHost);
+    fprintf(file, "%8.4f %8.5e %8.5e %8.5e \n", 
+	           energy, sigT, sigA, sigF);
   }
   fclose(file);
-
+  cudaFree(dev_sigT);
+  cudaFree(dev_sigA);
+  cudaFree(dev_sigF);
   return 0;
 }
