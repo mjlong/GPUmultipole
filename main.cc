4c8aa341d5638229bfecf65bec7af7e7e90e934f
jlmiao@nsecluster <jlmiao@mit.edu>
2014-04-26 15:24:46 -0400

    more test

--- main.cc
+++ main.cc
@@ -2,11 +2,15 @@
 #include "hdf5IO.h"
 #include "multipole_data.h"
 
+extern void anyvalue(struct multipoledata, int*, double *);
 int main(){
   char h5filename[] = "092238.h5";
-  struct multipoledata U238;
-  h5read(U238, h5filename);
-  U238.mpdata[0].output();
-  U238.mpdata[1].output();
+  struct multipoledata U238data;
+  int l;
+  double *ptr;
+  h5read(U238data, h5filename);
+  anyvalue(U238data,&l,ptr);
+  printf("numL=%d\n",l);
+  //  printf("pseudo_rhos\%g,%g\n",ptr[0],ptr[1]);
   return 0;
 }
