c44314171412285f5bc516d3ed6dea31ef73f46c
jlmiao@nsecluster <jlmiao@mit.edu>
2014-04-27 11:09:07 -0400

    test cudaMemcpy complex array

--- main.cc
+++ main.cc
@@ -2,15 +2,15 @@
 #include "hdf5IO.h"
 #include "multipole_data.h"
 
-extern void anyvalue(struct multipoledata, int*, double *);
+extern void anyvalue(struct multipoledata, int*, double *, double *);
 int main(){
   char h5filename[] = "092238.h5";
   struct multipoledata U238data;
   int l;
-  double d;
+  double d1, d2;
   h5read(U238data, h5filename);
-  anyvalue(U238data,&l,&d);
+  anyvalue(U238data,&l,&d1, &d2);
   printf("numL=%d\n",l);
-  printf("double=\%g\n",d);
+  printf("double=%10.6e and %10.6e\n",d1, d2);
   return 0;
 }
--- main.cu
+++ main.cu
@@ -7,14 +7,13 @@ __global__ void history();
 
 
 
-void anyvalue(struct multipoledata data, int *value, double *ptr){
-  double *prho, *plvalue, *pwstart, *pwend, *pfit;
-  CComplex  *psigTfactor, *pmpdata;
-  struct multipoledata *pdata;
+void anyvalue(struct multipoledata data, int *value, double *d1, double *d2){
   multipole U238(data);
+  CPUComplex z;
   cudaMemcpy(value, U238.l_value+25, sizeof(int), cudaMemcpyDeviceToHost);
-  cudaMemcpy(ptr  , U238.dev_doubles+SQRTAWR, sizeof(double), cudaMemcpyDeviceToHost);
-  //  ptr = U238.pseudo_rho;
+  cudaMemcpy(&z  , U238.mpdata+10, 2*sizeof(double), cudaMemcpyDeviceToHost);
+  *d1 = imag(z);
+  *d2 = imag(*(data.mpdata+10));
   dim3 dimBlock(1);
   dim3 dimGrid(10,10,10);
   history<<<dimBlock, dimGrid>>>();
