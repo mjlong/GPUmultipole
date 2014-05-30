607bf1603de7dda077490e2ffc3d614306c0be72
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-05-30 16:00:46 -0400

    only sigT_factor deserves being shared; save sharedmemory size by overwriting sigT_factor part when tallying

--- simulation.cu
+++ simulation.cu
@@ -1,14 +1,13 @@
 #include "simulation.h"
 
 __global__ void initialize(neutronInfo Info, double energy){
-  //  int id = ((blockDim.x*blockDim.y*blockDim.z)*(blockIdx.y*gridDim.x+blockIdx.x)+(blockDim.x*blockDim.y)*threadIdx.z+blockDim.x*threadIdx.y+threadIdx.x);//THREADID;
+  //int id = ((blockDim.x*blockDim.y*blockDim.z)*(blockIdx.y*gridDim.x+blockIdx.x)+(blockDim.x*blockDim.y)*threadIdx.z+blockDim.x*threadIdx.y+threadIdx.x);//THREADID;
   int id = blockDim.x * blockIdx.x + threadIdx.x;
   Info.energy[id] = energy; //id+1.0; //(id + 1)*1.63*energy*0.001;// 
 
 }
 
 __global__ void history(multipole U238, double *devicearray, struct neutronInfo Info){
-  //__global__ void history(multipole U238, struct neutronInfo Info){
   //TODO:this is one scheme to match threads to 1D array, 
   //try others when real simulation structure becomes clear
   int id = blockDim.x * blockIdx.x + threadIdx.x;//THREADID;
@@ -29,31 +28,34 @@ __global__ void history(multipole U238, double *devicearray, struct neutronInfo 
   localenergy = Info.energy[id];
   unsigned cnt = 0;
   unsigned idl = threadIdx.x;
-  unsigned idb = blockIdx.x;
   unsigned blocksize = blockDim.x;
   /*
     shift shared memory for double twophi[MAXNUML] and complex sigT_factor[MAXNUML]
   */
   //TODO: tailor to accomodate more than two isotopes
   sharedptr.blockbase   = Info.share.blockbase;
-  sharedptr.sigT_factor = (CComplex*)(shared + blocksize) + idl;
-  sharedptr.w_start     = (unsigned*)(shared + blocksize + (blocksize<<2)*Info.share.numL);
-  sharedptr.w_end       = sharedptr.w_start + Info.share.windows;
-
-  cnt = idl;
+  sharedptr.sigT_factor = (CComplex*)(shared) + idl;
   /*
+  //sharedptr.w_start     = (unsigned*)(shared + blocksize + (blocksize<<2)*Info.share.numL);
+  //sharedptr.w_end       = sharedptr.w_start + Info.share.windows;
+  cnt = idl;
   while(cnt<Info.share.windows){
     sharedptr.w_start[cnt] = U238.w_start[cnt];
     sharedptr.w_end[cnt]   = U238.w_end[cnt];
     cnt += blocksize;
-  }*/
+  }
   //__syncthreads();
-  cnt = 0;
 
-  sharedptr.dev_doubles =   (double*)(sharedptr.w_end + Info.share.windows);
-  if(idl<3)
-    sharedptr.dev_doubles[idl]  = U238.dev_doubles[idl];
+  //sharedptr.dev_doubles =  (double*)(shared + blocksize + (blocksize<<2)*Info.share.numL);
+    //(double*)(sharedptr.w_end + Info.share.windows);
+
+  if(3>idl){
+    //    for(cnt=0;cnt<3;cnt++)
+      sharedptr.dev_doubles[idl]  = U238.dev_doubles[idl];
+  }
   //__syncthreads();
+
+
 
   sharedptr.pseudo_rho  =   sharedptr.dev_doubles + 3;
   if(idl<Info.share.numL)
@@ -64,7 +66,9 @@ __global__ void history(multipole U238, double *devicearray, struct neutronInfo 
   if(idl<4)
     sharedptr.dev_integers[idl] = U238.dev_integers[idl];
   __syncthreads();
+  */
   
+  cnt = 0;
   while(live){
     rnd = curand_uniform(&localState);
     U238.xs_eval_fast(localenergy, sqrt(300.0*KB), sigT, sigA, sigF, sharedptr);
@@ -80,9 +84,10 @@ __global__ void history(multipole U238, double *devicearray, struct neutronInfo 
   devicearray[4*id+3]=sigF;
 
   /* Copy state back to global memory */ 
-  Info.rndState[id] = localState; 
+  //Info.rndState[id] = localState; 
 
   /*reduce tally*/
+  __syncthreads();
   unsigned *tally = (unsigned*)(shared);
   int i;
   tally[idl] = cnt;
@@ -97,7 +102,7 @@ __global__ void history(multipole U238, double *devicearray, struct neutronInfo 
   if(0==idl){
     //reduction scheme depends on tally type
     //following is to count moderation times
-    Info.ntally.cnt[idb] = tally[0];
+    Info.ntally.cnt[blockIdx.x] = tally[0];
   }
 
 }
