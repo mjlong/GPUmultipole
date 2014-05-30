3e468e7137b5975213d427787054f7e528b59bef
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-05-30 17:05:47 -0400

    clean up variables

--- simulation.cu
+++ simulation.cu
@@ -1,6 +1,7 @@
 #include "simulation.h" 
 __global__ void initialize(neutronInfo Info, double energy){
-  int id = ((blockDim.x*blockDim.y*blockDim.z)*(blockIdx.y*gridDim.x+blockIdx.x)+(blockDim.x*blockDim.y)*threadIdx.z+blockDim.x*threadIdx.y+threadIdx.x);//THREADID;
+  //int id = ((blockDim.x*blockDim.y*blockDim.z)*(blockIdx.y*gridDim.x+blockIdx.x)+(blockDim.x*blockDim.y)*threadIdx.z+blockDim.x*threadIdx.y+threadIdx.x);//THREADID;
+  int id = blockDim.x * blockIdx.x + threadIdx.x;
   Info.energy[id] = energy; //(id+1.0); //energy;//(id + 1)*1.63*energy*0.001;// 
 
 }
@@ -8,7 +9,7 @@ __global__ void initialize(neutronInfo Info, double energy){
 __global__ void history(multipole U238, double *devicearray, struct neutronInfo Info){
   //TODO:this is one scheme to match threads to 1D array, 
   //try others when real simulation structure becomes clear
-  int id = ((blockDim.x*blockDim.y*blockDim.z)*(blockIdx.y*gridDim.x+blockIdx.x)+(blockDim.x*blockDim.y)*threadIdx.z+blockDim.x*threadIdx.y+threadIdx.x);//THREADID;
+  int id = blockDim.x * blockIdx.x + threadIdx.x;
 
   bool live=true;
   double localenergy;
@@ -43,19 +44,11 @@ __global__ void history(multipole U238, double *devicearray, struct neutronInfo 
 
   /*reduce tally*/
   unsigned *tally = shared;
+  int idl = threadIdx.x;
   int i;
-  int idl = 
-    (blockDim.x*blockDim.y)*threadIdx.z+
-    blockDim.x*threadIdx.y+
-    threadIdx.x;
-
-  int idb = 
-    blockIdx.y*gridDim.x+blockIdx.x;
-  int blocksize = blockDim.x * blockDim.y * blockDim.z;
-
   tally[idl] = cnt;
   __syncthreads();
-  i = blocksize>>1;
+  i = blockDim.x>>1;
   while(i){
     if(idl<i)
       tally[idl] += tally[idl+i];
@@ -65,7 +58,7 @@ __global__ void history(multipole U238, double *devicearray, struct neutronInfo 
   if(0==idl){
     //reduction scheme depends on tally type
     //following is to count moderation times
-    Info.tally[idb] = tally[0];
+    Info.tally[blockIdx.x] = tally[0];
   }
 }
 
