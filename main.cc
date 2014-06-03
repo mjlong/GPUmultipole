12d1755d3dd6147d22512b527af4c7929e154e41
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-06-03 10:44:42 -0400

    not include hdf5read() but declare as extern in order to compile with nvcc and use global texture variable

--- main.cc
+++ main.cc
@@ -1,13 +1,12 @@
+#include "stdlib.h"
 #include "CPUComplex.h"
-#include "hdf5IO.h"
 #include "multipole_data.h"
 
+extern void h5read(struct multipoledata & pole, char filename[]);
 extern void anyvalue(struct multipoledata, int, int);
 int main(int argc, char *argv[]){
   char h5filename[] = "092238.h5";
   struct multipoledata U238data;
-  int l;
-  double d1, d2;
   h5read(U238data, h5filename);
   anyvalue(U238data, atoi(argv[1]),atoi(argv[2]));
   //  printf("numL=%d\n",l);
