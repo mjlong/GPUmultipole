e421c0b67c49ba613ec75c055af53537639b8fd3
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-06-03 17:48:12 -0400

    main program with nvcc; hdf5 reader as extern

--- main.cu
+++ main.cu
@@ -1,16 +1,12 @@
 #include "CPUComplex.h"
-#include "hdf5IO.h"
 #include "multipole_data.h"
 
+void h5read(struct multipoledata & pole, char filename[]);
 extern void anyvalue(struct multipoledata, int, int);
 int main(int argc, char *argv[]){
   char h5filename[] = "092238.h5";
   struct multipoledata U238data;
-  int l;
-  double d1, d2;
   h5read(U238data, h5filename);
   anyvalue(U238data, atoi(argv[1]), atoi(argv[2]));
-  //  printf("numL=%d\n",l);
-  //  printf("double=%10.6e and %10.6e\n",d1, d2);
   return 0;
 }
