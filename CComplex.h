f7f7b58afc80e4028e902864984e620023e23910
Jilang_Miao <jlmiao@mit.edu>
2014-06-06 00:29:51 -0400

    GPU version

--- CComplex.h
+++ CComplex.h
@@ -16,64 +16,283 @@ History****:*Date*******************Author**************************************
   Tailor to be compatible with general functions:
   real(complex),imag(complex),exp(complex);
   overload negative and minus operator simultaneously
+*****Thu Jun 5 22:47:23 2014 -0400  Jilang Miao 缪佶朗<jlmiao@mit.edu>
+  Make it a class template
 ================================================================================
 */
 #ifndef __CCOMPLEX_H__
 #define __CCOMPLEX_H__
 #include <iostream>
 #include <math.h>
-#include "CPUComplex.h"
 using namespace std;
 
-#define PI 3.141592653589793238462643383279
+#define PI 3.14159265358979323846264338327950
 
-
+template <class DType>
 class CComplex{
 private:
-  double m_real;
-  double m_imag;
+  DType m_real;
+  DType m_imag;
 
 public:
-  __host__ __device__ CComplex(double m_real=0.0, double m_imag=0.0f);
-  __host__ __device__ double Re();
-  __host__ __device__ double Im();
-  __host__ __device__ double Norm();
-  __host__ __device__ double Arg();
-  __host__ __device__ CComplex Conjugate();
-  //display() in form of norm*Exp(phi)
-  void display();
-  //output()  in form of real+(-)imag(*)i
-  void output();
-  __host__ __device__ ~CComplex();
-  __host__ __device__ CComplex operator + (CComplex);
-  __host__ __device__ CComplex operator + (double);
-  __host__ __device__ CComplex operator -  (double);
-  __host__ __device__ CComplex operator *  (CComplex);
-  __host__ __device__ CComplex operator *  (double);
+   __host__ __device__ CComplex(DType m_real=0.0, DType m_imag=0.0f);
+   __host__ __device__ DType Re();
+   __host__ __device__ DType Im();
+   __host__ __device__ DType Norm();
+   __host__ __device__ DType Arg();
+   __host__ __device__ CComplex Conjugate();
+   //display() in form of norm*Exp(phi)
+   __host__ __device__ void display();
+   //output()  in form of real+(-)imag(*)i
+   __host__ __device__ void output();
+   __host__ __device__ ~CComplex();
+   __host__ __device__ CComplex operator + (CComplex);
+   __host__ __device__ CComplex operator + (DType);
+   __host__ __device__ CComplex operator -  (DType);
+   __host__ __device__ CComplex operator *  (CComplex);
+   __host__ __device__ CComplex operator *  (DType);
   //division operator has been simplified by ignoring 0 divisor
-  //in case of being devided by double, it is suggested to multiply by 1/double
-  __host__ __device__ CComplex operator /   (CComplex);
-  __host__ __device__ CComplex operator /   (double);
+  //in case of being devided by DType, it is suggested to multiply by 1/DType
+   __host__ __device__ CComplex operator /   (CComplex);
+   __host__ __device__ CComplex operator /   (DType);
   //	CComplex & operator =  (const CComplex & c);
 
 };
 
-__host__ __device__ double real(CComplex);
-__host__ __device__ double imag(CComplex);
-__host__ __device__ double Norm(CComplex);
-__host__ __device__ double Arg(CComplex);
-__host__ __device__ CComplex Conjugate(CComplex);
-__host__ __device__ CComplex exp(CComplex);
-void display(CComplex);
-void output(CComplex);
+template <class DType>
+__host__ __device__ CComplex<DType>::CComplex(DType real, DType imag){
+  m_real=real;
+  m_imag=imag;
+}
 
-__host__ __device__ CComplex operator + (double d, CComplex c);
-__host__ __device__ CComplex operator - (double d, CComplex c);
-__host__ __device__ CComplex operator -  (CComplex);
-//It should be noted that the '-' here is negative sign
-__host__ __device__ CComplex operator - (CComplex c1, CComplex c2);
-__host__ __device__ CComplex operator * (double d, CComplex c);
-__host__ __device__ CComplex operator / (double d, CComplex c);
 
+template <class DType>
+__host__ __device__ DType CComplex<DType>::Re(){
+  return m_real;
+}
+
+template <class DType>
+__host__ __device__ DType real(CComplex<DType> c){
+  return c.Re();
+}
+
+template <class DType>
+__host__ __device__ DType CComplex<DType>::Im(){
+  return m_imag;
+}
+
+
+
+template <class DType>
+__host__ __device__ DType imag(CComplex<DType> c){
+  return c.Im();
+}
+
+template <class DType>
+__host__ __device__ DType CComplex<DType>::Norm(){
+  return sqrt( m_real*m_real + m_imag*m_imag);
+}
+
+template <class DType>
+__host__ __device__ DType Norm(CComplex<DType> c){
+  DType x = c.Re();
+  DType y = c.Im();
+  return sqrt( x*x + y*y);
+}
+
+template <class DType>
+__host__ __device__ DType CComplex<DType>::Arg(){
+  if ( 0== m_real){
+    if( m_imag>0) return PI*0.5;
+    else if (m_imag<0) return PI*1.5;
+    else return 0.0;
+  }
+  else{
+    if(0==m_imag){
+      return m_real>0 ? 0.0 : PI;
+    }
+    else{
+      if ( m_real <0) return atan(m_imag/m_real )+PI;
+      else return m_imag>0 ? atan(m_imag/m_real) : atan(m_imag/m_real) +2.0*PI;
+    }
+  }
+}
+
+
+template <class DType>
+__host__ __device__ DType Arg(CComplex<DType> c){
+  DType x = c.Re();
+  DType y = c.Im();
+  if ( 0 == x){
+    if( y>0) return PI*0.5;
+    else if (y<0) return PI*1.5;
+    else return 0.0;
+  }
+  else{
+    if(0==y){
+      return x>0 ? 0.0 : PI;
+    }
+    else{
+      if ( x <0) return atan(y/x )+PI;
+      else return y>0 ? atan(y/x) : atan(y/x) +2.0*PI;
+    }
+  }
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType>::Conjugate(){
+  return CComplex(m_real, 0.0 - m_imag);
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> Conjugate(CComplex<DType> c){
+  return CComplex<DType>(c.Re(), 0.0 - c.Im());
+}
+
+
+template <class DType>
+__host__ __device__ CComplex<DType> exp(CComplex<DType> c){
+  DType y = c.Im();
+  return exp(c.Re())*(cos(y)*sin(y));
+}
+
+template <class DType>
+__host__ __device__ void CComplex<DType>::output(){
+  if(0==m_real){
+    if(0==m_imag) cout<<m_real<<endl;
+    else cout<<m_imag<<"i"<<endl;
+  }
+  else{
+    if(0==m_imag) cout<<m_real<<endl;
+    else{
+      if(m_imag>0)   cout<<m_real<<"+"<<m_imag<<"i"<<endl;
+      else cout<<m_real<<"-"<<0-m_imag<<"i"<<endl;
+    }
+  }
+}
+
+template <class DType>
+__host__ __device__ void output(CComplex<DType> c){
+  if(0==c.Re()){
+    if(0==c.Im()) cout<<c.Re()<<endl;
+    else cout<<c.Im()<<"i"<<endl;
+  }
+  else{
+    if(0==c.Im()) cout<<c.Re()<<endl;
+    else{
+      if(c.Im()>0)   cout<<c.Re()<<"+"<<c.Im()<<"i"<<endl;
+      else cout<<c.Re()<<"-"<<0-c.Im()<<"i"<<endl;
+    }
+  }
+}
+
+template <class DType>
+__host__ __device__ void CComplex<DType>::display(){
+  DType norm = Norm();
+  DType arg  = Arg();
+  if(0==norm) cout<<norm<<endl;
+  else cout<<norm<<"*Exp("<<arg<<")"<<endl;
+}
+
+template <class DType>
+__host__ __device__ void display(CComplex<DType> c){
+  DType norm = c.Norm();
+  DType arg  = c.Arg();
+  if(0==norm) cout<<norm<<endl;
+  else cout<<norm<<"*Exp("<<arg<<")"<<endl;
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType>::operator +(CComplex<DType> c){
+   return CComplex( m_real+c.Re(), m_imag+c.Im());
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType>::operator +(DType d){
+  return CComplex(m_real+d, m_imag);
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType>::operator - (DType d) {
+  return CComplex(m_real-d, m_imag);
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType> ::operator * (CComplex<DType> c) {
+   DType real=m_real*c.Re()-m_imag*c.Im();
+   DType imag=c.Re()*m_imag+m_real*c.Im();
+  return CComplex(real, imag);
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType> :: operator * (DType d){
+  return CComplex( m_real*d, m_imag*d);
+}
+
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType>::operator / (DType d) {
+  return CComplex( m_real/ d, m_imag/d);
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> CComplex<DType> ::operator /(CComplex<DType> c) {
+  DType r=c.Norm() * c.Norm();
+  CComplex<DType> c1(m_real, m_imag);
+  CComplex<DType> c2(c.Re(), 0.0 - c.Im());
+  CComplex<DType> c3=c1*c2;
+  return c3/r;
+
+}
+
+
+/*
+CComplex & CComplex ::operator =(const CComplex &c_right){
+	if( this==&c_right) return *this;
+	m_real=c_right.m_real;
+	m_imag=c_right.m_imag;
+	m_NA=c_right.m_NA;
+
+	return *this;
+}*/
+
+
+template <class DType>
+__host__ __device__ CComplex<DType>::~CComplex(){
+
+}
+
+
+
+template <class DType>
+__host__ __device__ CComplex<DType> operator + (DType d, CComplex<DType> c){
+  return CComplex<DType>( d+c.Re(), c.Im());
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> operator - (DType d, CComplex<DType> c) {
+  return CComplex<DType> (d-c.Re(), -c.Im());
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> operator - (CComplex<DType> c1, CComplex<DType> c2) {
+  return CComplex<DType> (c1.Re()-c2.Re(), c1.Im()-c2.Im());
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> operator - (CComplex<DType> c){
+  return CComplex<DType>(-c.Re(), -c.Im());
+}
+
+
+template <class DType>
+__host__ __device__ CComplex<DType> operator * (DType d, CComplex<DType> c){
+  return CComplex<DType> (d*c.Re(), d*c.Im());
+}
+
+template <class DType>
+__host__ __device__ CComplex<DType> operator / (DType d, CComplex<DType> c){
+  return d*c.Conjugate() / (c.Norm() *c.Norm() );
+}
 
 #endif
