91c2635e718f6b029947f49c64da4a10c36f4a6e
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-06-20 16:24:48 -0400

    no sampling in this stage

--- simulation.cu
+++ simulation.cu
@@ -21,7 +21,8 @@ __global__ void history(multipole U238, MemStruct Info, unsigned num_src, unsign
   unsigned istep;
   bool live=true;
   double localenergy;
-  double rnd,norm;
+  double rnd;
+  //double norm;
   double sigT, sigA, sigF;
 
   /* Each thread gets same seed, a different sequence number, no offset */
@@ -36,8 +37,9 @@ __global__ void history(multipole U238, MemStruct Info, unsigned num_src, unsign
   //while(live){
   for (istep = 0; istep < devstep; istep++){
     rnd = curand_uniform(&localState);
-    norm = curand_normal(&localState);
-    U238.xs_eval_fast(localenergy, sqrt(300.0*KB), norm, sigT, sigA, sigF);
+    //norm = curand_normal(&localState);
+    //U238.xs_eval_fast(localenergy, sqrt(300.0*KB), norm, sigT, sigA, sigF);
+    U238.xs_eval_fast(localenergy, sqrt(300.0*KB), sigT, sigA, sigF);
     localenergy = localenergy * rnd;
     live = (localenergy > 1.0);
     cnt = cnt + 1;
@@ -63,7 +65,8 @@ __global__ void remaining(multipole U238, double *devicearray, MemStruct Info){
   int id = blockDim.x * blockIdx.x + threadIdx.x;
   bool live = true;
   double localenergy;
-  double rnd, norm;
+  double rnd;
+  //double norm;
   double sigT, sigA, sigF;
   
   /* Each thread gets same seed, a different sequence number, no offset */
@@ -77,8 +80,9 @@ __global__ void remaining(multipole U238, double *devicearray, MemStruct Info){
   unsigned terminated = 0u;
   while(live){
     rnd = curand_uniform(&localState);
-    norm = curand_normal(&localState);
-    U238.xs_eval_fast(localenergy, sqrt(300.0*KB), norm, sigT, sigA, sigF);
+    //norm = curand_normal(&localState);
+    //U238.xs_eval_fast(localenergy, sqrt(300.0*KB), norm, sigT, sigA, sigF);
+    U238.xs_eval_fast(localenergy, sqrt(300.0*KB), sigT, sigA, sigF);
     localenergy = localenergy * rnd;
     live = (localenergy > 1.0);
     cnt = cnt + 1;
