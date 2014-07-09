dc894e84f72dd7753b0046f1ef5d88c3341155ab
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-07-09 18:10:23 -0400

    support float

--- src/wfunction/fourierw.cu
+++ src/wfunction/fourierw.cu
@@ -7,10 +7,10 @@ __device__ CComplex<CMPTYPE> fourierw(CComplex<CMPTYPE> z, CMPTYPE taom, unsigne
   CComplex<CMPTYPE> C = A*A;
   w = CComplex<CMPTYPE>((CMPTYPE)0.0,(CMPTYPE)0.0); 
   for(int n=1;n<=M;n++){
-    w = w + exp(-n*n*PI*PI/(taom*taom))*( (CMPTYPE)(((n<<31)>>31)|0x00000001)*B - (CMPTYPE)1.0)/(n*n*PI*PI - C); 
+    w = w + (CMPTYPE)exp(-n*n*PI*PI/(taom*taom))*( (CMPTYPE)(((n<<31)>>31)|0x00000001)*B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C); 
   }  
   w = w*2.0*A;
-  w = w + (1.0-B)/(taom*z);
+  w = w + ((CMPTYPE)1.0-B)/(taom*z);
   w = ONEI*w;
   return w;
 }
