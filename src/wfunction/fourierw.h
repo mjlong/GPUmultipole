8368fb21b97d3310c67b0d8c4a64ba28d808c4cb
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-07-09 17:58:19 -0400

    clean up:

--- src/wfunction/fourierw.h
+++ src/wfunction/fourierw.h
@@ -1,13 +1,6 @@
 #ifndef __FOURIER_H__
 #define __FOURIER_H__
 
-#if defined(__CFLOAT)
-#define CMPTYPE float
-#define CMPTYPE2 float2
-#else
-#define CMPTYPE double 
-#define CMPTYPE2 double2
-#endif
 
 #include <stdio.h>
 #include "global.h"
