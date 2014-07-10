016833b77e2097a491a74bee4d3252723aaf6464
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-07-10 07:41:03 -0400

    add __global__ kernel to evaluate coefficients in advance

--- src/wfunction/fourierw.cu
+++ src/wfunction/fourierw.cu
@@ -1,5 +1,14 @@
 #include "fourierw.h"
 
+extern __constant__ CMPTYPE a[];
+
+__global__ void fill_a(CMPTYPE *a){
+  // since n is supposed to be 1,2,3,...,23
+  // by default dimGrid=(1,1,1)
+  int n = threadIdx.x+1;
+  a[n] = exp(-n*n*PI*PI/(taom*taom));
+}
+
 __device__ CComplex<CMPTYPE> w_function(CComplex<CMPTYPE> z){
   CComplex<CMPTYPE> w;
   CComplex<CMPTYPE> A = taom*z;
@@ -8,11 +17,11 @@ __device__ CComplex<CMPTYPE> w_function(CComplex<CMPTYPE> z){
   w = CComplex<CMPTYPE>((CMPTYPE)0.0,(CMPTYPE)0.0); 
   for(int n=1;n<M;n++){
     //w = w + exp(-n*n*PI*PI/(taom*taom))*( (CMPTYPE)(((n<<31)>>31)|0x00000001)*B - (CMPTYPE)1.0)/(n*n*PI*PI - C);
-    w = w + (CMPTYPE)exp(-n*n*PI*PI/(taom*taom))*( -B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C);
+    w = w + a[n]*( -B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C);
     n++;
-    w = w + (CMPTYPE)exp(-n*n*PI*PI/(taom*taom))*(  B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C);
+    w = w + a[n]*(  B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C);
   }
-  w = w + (CMPTYPE)exp(-23*23*PI*PI/(taom*taom))*( -B - (CMPTYPE)1.0)/((CMPTYPE)(23*23*PI*PI) - C);
+  w = w + a[23] *( -B - (CMPTYPE)1.0)/((CMPTYPE)(23*23*PI*PI) - C);
 
   w = w*2.0*A;
   w = w + ((CMPTYPE)1.0-B)/(taom*z);
--- src/wfunction/fourierw.h
+++ src/wfunction/fourierw.h
@@ -8,6 +8,7 @@
 
 #define taom 12.0
 #define M    23
+__global__ void fill_a(CMPTYPE* a);
 __device__ CComplex<CMPTYPE> w_function(CComplex<CMPTYPE> z);
 
 
