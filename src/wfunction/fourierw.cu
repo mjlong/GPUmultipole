348d5fb291342ea8b362d11a8459f0be44d65b44
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-07-10 00:28:25 -0400

    finally the fast (-1)^n evaluation is split 1 line to 2

--- src/wfunction/fourierw.cu
+++ src/wfunction/fourierw.cu
@@ -6,9 +6,14 @@ __device__ CComplex<CMPTYPE> fourierw(CComplex<CMPTYPE> z, CMPTYPE taom, unsigne
   CComplex<CMPTYPE> B = exp(ONEI*A);
   CComplex<CMPTYPE> C = A*A;
   w = CComplex<CMPTYPE>((CMPTYPE)0.0,(CMPTYPE)0.0); 
-  for(int n=1;n<=M;n++){
-    w = w + (CMPTYPE)exp(-n*n*PI*PI/(taom*taom))*( (CMPTYPE)(((n<<31)>>31)|0x00000001)*B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C); 
-  }  
+  for(int n=1;n<M;n++){
+    //w = w + exp(-n*n*PI*PI/(taom*taom))*( (CMPTYPE)(((n<<31)>>31)|0x00000001)*B - (CMPTYPE)1.0)/(n*n*PI*PI - C);
+    w = w + (CMPTYPE)exp(-n*n*PI*PI/(taom*taom))*( -B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C);
+    n++;
+    w = w + (CMPTYPE)exp(-n*n*PI*PI/(taom*taom))*(  B - (CMPTYPE)1.0)/((CMPTYPE)(n*n*PI*PI) - C);
+  }
+  w = w + (CMPTYPE)exp(-23*23*PI*PI/(taom*taom))*( -B - (CMPTYPE)1.0)/((CMPTYPE)(23*23*PI*PI) - C);
+
   w = w*2.0*A;
   w = w + ((CMPTYPE)1.0-B)/(taom*z);
   w = ONEI*w;
