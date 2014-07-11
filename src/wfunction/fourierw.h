260e9eb1152c7c5a599e1318367cf48e273fe258
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-07-11 11:49:18 -0400

    loosen taom and N further

--- src/wfunction/fourierw.h
+++ src/wfunction/fourierw.h
@@ -7,12 +7,12 @@
 #include "CComplex.h"
 
 #if defined(__CFLOAT)
-#define taom 9.0f //12.0f
+#define taom 7.1f //6.0f //9.0f //12.0f
 #else
-#define taom 9.0  //12.0
+#define taom 7.1 //6.0 // 9.0  //12.0
 #endif
 
-#define M    13   //23
+#define M    8 //6 //13   //23
 __global__ void fill_a(CMPTYPE* a, CMPTYPE* b);
 __device__ CComplex<CMPTYPE> w_function(CComplex<CMPTYPE> z);
 
