aa22fa79f158626655dfa7171214477fcd870d4e
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-08-01 09:55:38 -0400

    multi isotopes reading

--- src/main.cu
+++ src/main.cu
@@ -13,11 +13,15 @@ __constant__ double2 table[LENGTH][LENGTH];
 #endif
 */
 extern void h5read(struct multipoledata & pole, char filename[]);
-extern void anyvalue(struct multipoledata, unsigned, unsigned, unsigned, unsigned);
+extern void anyvalue(struct multipoledata*,unsigned, unsigned, unsigned, unsigned, unsigned);
 int main(int argc, char *argv[]){
+  unsigned numIso=2;
   char h5filename[] = "092238.h5";
-  struct multipoledata U238data;
-  h5read(U238data, h5filename);
-  anyvalue(U238data, atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
+  struct multipoledata *isotopes;//U238data;
+  isotopes = (struct multipoledata*)malloc(sizeof(struct multipoledata)*numIso);
+  for(int i=0;i<numIso;i++){
+    h5read(isotopes[i], h5filename);
+  }
+  anyvalue(isotopes,numIso, atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
   return 0;
 }
