e12c564478b9fde07c4505f0c176eddb31406806
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-08-02 12:50:19 -0400

    move filenames reading into function

--- src/main.cu
+++ src/main.cu
@@ -18,16 +18,30 @@ __constant__ double2 table[LENGTH][LENGTH];
 */
 extern void h5read(struct multipoledata & pole, char filename[]);
 extern void anyvalue(struct multipoledata*,unsigned, unsigned, unsigned, unsigned, unsigned);
+int init_data(char* input, char filenames[][FILENAMELEN]);
 
 int main(int argc, char **argv){
+  int numIso;
+  char filenames[MAXISOTOPES][FILENAMELEN];
+  numIso = init_data(argv[5],filenames);
+  struct multipoledata *isotopes;
+  isotopes = (struct multipoledata*)malloc(sizeof(struct multipoledata)*numIso);
+  for(int i=0;i<numIso;i++)  
+    h5read(isotopes[i],filenames[i]);
+
+  anyvalue(isotopes,numIso, atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
+  free(isotopes);
+ 
+  return 0;
+}
+
+int init_data(char* input, char line[][FILENAMELEN]){
   int numIso=-1;
   FILE *fp = NULL;
-  char line[MAXISOTOPES][FILENAMELEN];
-  struct multipoledata *isotopes;//U238data;
-  fp = fopen(argv[5],"r");
+  fp = fopen(input,"r");
 
   if (fp == NULL) {
-    fprintf(stderr, "Can't open input file in.list!\n");
+    fprintf(stderr, "Can't open input file %s!\n",input);
     exit(1);
   }
   int i;
@@ -38,12 +52,6 @@ int main(int argc, char **argv){
     line[numIso][i]='\0';
     printf("\n"); 
   }
-  isotopes = (struct multipoledata*)malloc(sizeof(struct multipoledata)*numIso);
-  for(i=0;i<numIso;i++)  
-    h5read(isotopes[i],line[i]);
-
-  anyvalue(isotopes,numIso, atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
-  free(isotopes);
   fclose(fp);
-  return 0;
+  return numIso;
 }
