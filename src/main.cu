699bd6fa25d7a07335c3ffeb77b0e6ada73f99e1
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-08-02 12:30:12 -0400

    nuclear data file name read from file

--- src/main.cu
+++ src/main.cu
@@ -2,7 +2,11 @@
 #include "CComplex.h"
 #include "multipole_data.h"
 #include "global.h"
+#include <stdio.h>
+#include <string.h>
 
+#define FILENAMELEN 20
+#define MAXISOTOPES 10
 /*#if defined (__QUICKWC)
 #if defined(__CFLOAT)
 __constant__ float2 table[LENGTH][LENGTH];
@@ -14,15 +18,32 @@ __constant__ double2 table[LENGTH][LENGTH];
 */
 extern void h5read(struct multipoledata & pole, char filename[]);
 extern void anyvalue(struct multipoledata*,unsigned, unsigned, unsigned, unsigned, unsigned);
-int main(int argc, char *argv[]){
-  unsigned numIso=1;
-  char h5filename[] = "092238.h5";
+
+int main(int argc, char **argv){
+  int numIso=-1;
+  FILE *fp = NULL;
+  char line[MAXISOTOPES][FILENAMELEN];
   struct multipoledata *isotopes;//U238data;
-  isotopes = (struct multipoledata*)malloc(sizeof(struct multipoledata)*numIso);
-  for(int i=0;i<numIso;i++){
-    h5read(isotopes[i], h5filename);
+  fp = fopen(argv[5],"r");
+
+  if (fp == NULL) {
+    fprintf(stderr, "Can't open input file in.list!\n");
+    exit(1);
   }
+  int i;
+  while(fgets(line[++numIso], FILENAMELEN, fp) != NULL) {
+    i=-1;
+    printf("%s", line[numIso]);
+    while('\n'!=line[numIso][++i]){}
+    line[numIso][i]='\0';
+    printf("\n"); 
+  }
+  isotopes = (struct multipoledata*)malloc(sizeof(struct multipoledata)*numIso);
+  for(i=0;i<numIso;i++)  
+    h5read(isotopes[i],line[i]);
+
   anyvalue(isotopes,numIso, atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
   free(isotopes);
+  fclose(fp);
   return 0;
 }
