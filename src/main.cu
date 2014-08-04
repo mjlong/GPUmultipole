b2e234e36b07d065d2ffb3ff1379ccaddabc0d3f
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-08-04 09:46:09 -0400

    host multipoledata free on devicemain.cu after copied to device

--- src/main.cu
+++ src/main.cu
@@ -30,7 +30,6 @@ int main(int argc, char **argv){
     h5read(isotopes[i],filenames[i]);
 
   anyvalue(isotopes,numIso, atoi(argv[1]),atoi(argv[2]),atoi(argv[3]),atoi(argv[4]));
-  free(isotopes);
  
   return 0;
 }
