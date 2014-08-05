e39dfb4ed7a55ea528e83b2d104879fa04ddabad
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-08-05 16:28:39 -0400

    support more isotopes by saving one type of data eg. dev_integers, fitT in a long vector; allocate and assign the device pointers in creator; calculate offsets for each isotope at the sametime; 2(=count(FITT,FITA) defined as macro FIT_F

--- src/multipole.cu
+++ src/multipole.cu
@@ -11,6 +11,9 @@ multipole::multipole(struct multipoledata *data, int numIso){
 #endif //Only __QUICWG needs a global wtable
 
   size_t size;
+  int i;
+  int * h_offset = (int*)malloc(sizeof(int)*numIso);
+  int * h_size   = (int*)malloc(sizeof(int)*numIso);
   // allocate array of offsets
   size = sizeof(int)*numIso*NUMOFFS;
   gpuErrchk(cudaMalloc((void**)&offsets, size));
@@ -19,7 +22,7 @@ multipole::multipole(struct multipoledata *data, int numIso){
   */
   size = sizeof(unsigned);
   gpuErrchk(cudaMalloc((void**)&dev_integers, DEVINTS*size*numIso));
-  for(int i=0;i<numIso;i++){
+  for(i=0;i<numIso;i++){
     cudaMemcpy(dev_integers+i*DEVINTS+MODE,        &(data[i].mode), size, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_integers+i*DEVINTS+FITORDER,    &(data[i].fitorder), size, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_integers+i*DEVINTS+NUML,        &(data[i].numL), size, cudaMemcpyHostToDevice);
@@ -31,7 +34,7 @@ multipole::multipole(struct multipoledata *data, int numIso){
   */
   size = sizeof(CMPTYPE);
   cudaMalloc((void**)&dev_doubles,  DEVREALS*size*numIso);
-  for(int i=0;i<numIso;i++){
+  for(i=0;i<numIso;i++){
     cudaMemcpy(dev_doubles+i*DEVINTS+STARTE,  &(data[i].startE),  size, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_doubles+i*DEVINTS+SPACING ,&(data[i].spacing), size, cudaMemcpyHostToDevice);
     cudaMemcpy(dev_doubles+i*DEVINTS+SQRTAWR, &(data[i].sqrtAWR), size, cudaMemcpyHostToDevice);
@@ -40,64 +43,146 @@ multipole::multipole(struct multipoledata *data, int numIso){
   /*
     allocate and assign arrays
   */
-  size = data.length*(MP_RF+data.fissionable)*2*sizeof(CMPTYPE);
+  // mpdata
+  size = 0;
+  h_offset[0] = 0;
+  for(i=0;i<numIso-1;i++){
+    h_size[i] = data[i].length*(MP_RF+data[i].fissionable);
+    h_offset[i+1]=h_offset[i] + h_size[i];
+    h_size[i]*=2*sizeof(CMPTYPE);
+    size += h_size[i];
+  }
+    h_size[i] = data[i].length*(MP_RF+data[i].fissionable)*2*sizeof(CMPTYPE);
+    size += h_size[i];
   cudaMalloc((void**)&mpdata, size);
-  cudaMemcpy(mpdata, data.mpdata, size, cudaMemcpyHostToDevice);
+  for(i=0;i<numIso;i++){
+    cudaMemcpy(mpdata + h_offset[i], data[i].mpdata, h_size[i], cudaMemcpyHostToDevice);
+  }
+  cudaMemcpy(offsets+PMPDATA*numIso, h_offset, sizeof(int)*numIso, cudaMemcpyHostToDevice);
 
-  size = data.length*sizeof(unsigned);
+  // l_value
+  size = 0;
+  h_offset[0] = 0;
+  for(i=0;i<numIso-1;i++){
+    h_size[i] = data[i].length;
+    h_offset[i+1]=h_offset[i] + h_size[i];
+    h_size[i]*=sizeof(unsigned);
+    size += h_size[i];
+  }
+    h_size[i] = data[i].length*sizeof(unsigned);
+    size += h_size[i];
   cudaMalloc((void**)&l_value, size);
-  cudaMemcpy(l_value, data.l_value, size, cudaMemcpyHostToDevice);
+  for(i=0;i<numIso;i++){
+    cudaMemcpy(l_value + h_offset[i], data[i].l_value, h_size[i], cudaMemcpyHostToDevice);
+  }
+  cudaMemcpy(offsets+PLVAL*numIso, h_offset, sizeof(int)*numIso, cudaMemcpyHostToDevice);
 
-  size = data.numL*sizeof(CMPTYPE);
+  // pseudo_rho
+  size = 0;
+  h_offset[0] = 0;
+  for(i=0;i<numIso-1;i++){
+    h_size[i] = data[i].numL;
+    h_offset[i+1]=h_offset[i] + h_size[i];
+    h_size[i]*=sizeof(CMPTYPE);
+    size += h_size[i];
+  }
+    h_size[i] = data[i].numL*sizeof(CMPTYPE);
+    size += h_size[i];
   cudaMalloc((void**)&pseudo_rho, size);
-  cudaMemcpy(pseudo_rho, data.pseudo_rho, size, cudaMemcpyHostToDevice);
+  for(i=0;i<numIso;i++){
+    cudaMemcpy(pseudo_rho + h_offset[i], data[i].pseudo_rho, h_size[i], cudaMemcpyHostToDevice);
+  }
+  cudaMemcpy(offsets+PPRHO*numIso, h_offset, sizeof(int)*numIso, cudaMemcpyHostToDevice);
 
-
-  size = data.windows*sizeof(int);
+  // w_start and w_end
+  size = 0;
+  h_offset[0] = 0;
+  for(i=0;i<numIso-1;i++){
+    h_size[i] = data[i].windows;
+    h_offset[i+1]=h_offset[i] + h_size[i];
+    h_size[i]*=sizeof(int);
+    size += h_size[i];
+  }
+    h_size[i] = data[i].windows*sizeof(int);
+    size += h_size[i];
   cudaMalloc((void**)&w_start, size);
-  cudaMemcpy(w_start, data.w_start, size, cudaMemcpyHostToDevice);
-  cudaMalloc((void**)&w_end, size);
-  cudaMemcpy(w_end, data.w_end, size, cudaMemcpyHostToDevice);
+  cudaMalloc((void**)&w_end,   size);
+  for(i=0;i<numIso;i++){
+    cudaMemcpy(w_start + h_offset[i], data[i].w_start, h_size[i], cudaMemcpyHostToDevice);
+    cudaMemcpy(w_end   + h_offset[i], data[i].w_end,   h_size[i], cudaMemcpyHostToDevice);
+  }
+  cudaMemcpy(offsets+PWIND*numIso, h_offset, sizeof(int)*numIso, cudaMemcpyHostToDevice);
 
-  size = (data.fitorder+1)*data.windows*sizeof(CMPTYPE);
-  //cudaMalloc((void**)&fit, size);
-  //cudaMemcpy(fit, data.fit, size, cudaMemcpyHostToDevice);
+  // fitT fitA and fitF
+  size = 0;
+  h_offset[0] = 0;
+  for(i=0;i<numIso-1;i++){
+    h_size[i] = data[i].windows*(data[i].fitorder+1);
+    h_offset[i+1]=h_offset[i] + h_size[i];
+    h_size[i]*=sizeof(CMPTYPE);
+    size += h_size[i];
+  }
+    h_size[i] = data[i].windows*(data[i].fitorder+1)*2*sizeof(CMPTYPE);
+    size += h_size[i];
+  cudaMalloc((void**)&fitT, size);
+  cudaMalloc((void**)&fitA, size);
+  cudaMemcpy(offsets+PFITS*numIso, h_offset, sizeof(int)*numIso, cudaMemcpyHostToDevice);
+
+  CMPTYPE *h_fitT;
+  CMPTYPE *h_fitA; 
   unsigned ic, iw;
-  CMPTYPE *h_fitT = (CMPTYPE*)malloc(size);
-  CMPTYPE *h_fitA = (CMPTYPE*)malloc(size);
+  for(i=0;i<numIso;i++){
+    size = h_size[i]; 
+    h_fitT = (CMPTYPE*)malloc(size);
+    h_fitA = (CMPTYPE*)malloc(size);
+    for(ic=0;ic<=data[i].fitorder;ic++){
+      for(iw=0;iw<data[i].windows;iw++){
+        h_fitT[ic*data[i].windows+iw] = data[i].fit[findex(iw,ic,FIT_T,data[i].fitorder+1,FIT_F+data[i].fissionable)]; 
+     }
+    }
+    for(ic=0;ic<=data[i].fitorder;ic++){
+      for(iw=0;iw<data[i].windows;iw++){
+        h_fitA[ic*data[i].windows+iw] = data[i].fit[findex(iw,ic,FIT_A,data[i].fitorder+1,FIT_F+data[i].fissionable)]; 
+     }
+    }
+    gpuErrchk(cudaMemcpy(fitT+h_offset[i],h_fitT,size,cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(fitA+h_offset[i],h_fitT,size,cudaMemcpyHostToDevice));
+    free(h_fitT);
+    free(h_fitA);
+  }
+//fitF 
+  size = 0;
+  h_offset[0] = 0;
+  for(i=0;i<numIso-1;i++){
+    h_size[i] = data[i].fissionable? data[i].windows*(data[i].fitorder+1):0;
+    h_offset[i+1]=h_offset[i] + h_size[i];
+    h_size[i]*=sizeof(CMPTYPE);
+    size += h_size[i];
+  }
+    h_size[i] = data[i].fissionable? data[i].windows*(data[i].fitorder+1)*sizeof(CMPTYPE):0;
+    size += h_size[i];
+  cudaMalloc((void**)&fitF, size);
+  cudaMemcpy(offsets+PFITF*numIso, h_offset, sizeof(int)*numIso, cudaMemcpyHostToDevice);
+
+
   CMPTYPE *h_fitF ;
-  if(data.fissionable)
+  for(i=0;i<numIso;i++){
+    size = h_size[i]; 
+    if(0!=size){
     h_fitF = (CMPTYPE*)malloc(size);
-  for(ic=0;ic<=data.fitorder;ic++){
-    for(iw=0;iw<data.windows;iw++){
-      h_fitT[ic*data.windows+iw] = data.fit[findex(iw,ic,FIT_T,data.fitorder+1,2+data.fissionable)]; 
-   }
-  }
-  for(ic=0;ic<=data.fitorder;ic++){
-    for(iw=0;iw<data.windows;iw++){
-      h_fitA[ic*data.windows+iw] = data.fit[findex(iw,ic,FIT_A,data.fitorder+1,2+data.fissionable)]; 
-   }
-  }
-  if(data.fissionable){
-  for(ic=0;ic<=data.fitorder;ic++){
-    for(iw=0;iw<data.windows;iw++){
-      h_fitF[ic*data.windows+iw] = data.fit[findex(iw,ic,FIT_F,data.fitorder+1,2+data.fissionable)]; 
-   }
-  }
+    for(ic=0;ic<=data[i].fitorder;ic++){
+      for(iw=0;iw<data[i].windows;iw++){
+        h_fitF[ic*data[i].windows+iw] = data[i].fit[findex(iw,ic,FIT_F,data[i].fitorder+1,FIT_F+data[i].fissionable)]; 
+     }
+    }
+    gpuErrchk(cudaMemcpy(fitF+h_offset[i],h_fitF,size,cudaMemcpyHostToDevice));
+    free(h_fitF);
+    }
   }
 
-  gpuErrchk(cudaMalloc((void**)&fitT, size));
-  gpuErrchk(cudaMemcpy(fitT,h_fitT,size,cudaMemcpyHostToDevice));
-  free(h_fitT);
-  gpuErrchk(cudaMalloc((void**)&fitA, size));
-  gpuErrchk(cudaMemcpy(fitA,h_fitA,size,cudaMemcpyHostToDevice));
-  free(h_fitA);
-  if(data.fissionable) {
-    gpuErrchk(cudaMalloc((void**)&fitF, size));
-    gpuErrchk(cudaMemcpy(fitF,h_fitF,size,cudaMemcpyHostToDevice));
-    free(h_fitF);
-  }
- 
+
+  free(h_offset); 
+  free(h_size);
  
 #if defined(__QUICKWG)
   mtable = wtable;  
@@ -109,6 +194,7 @@ multipole::~multipole(){
 }
 
 void multipole::release_pointer(){
+  gpuErrchk(cudaFree(offsets));
   gpuErrchk(cudaFree(dev_integers));
   gpuErrchk(cudaFree(dev_doubles));
   gpuErrchk(cudaFree(mpdata));
@@ -336,10 +422,10 @@ __device__  void multipole::xs_eval_fast(CMPTYPE E,
 
   for (iC=0;iC<=fitorder;iC++){
     power = (CMPTYPE)pow((double)E,(double)iC*0.5-1.0);
-    sigT += fit[findex(iW,iC,FIT_T,fitorder+1,2+fissionable)]*power;
-    sigA += fit[findex(iW,iC,FIT_A,fitorder+1,2+fissionable)]*power;
+    sigT += fit[findex(iW,iC,FIT_T,fitorder+1,FIT_F+fissionable)]*power;
+    sigA += fit[findex(iW,iC,FIT_A,fitorder+1,FIT_F+fissionable)]*power;
     if(MP_FISS == fissionable)
-      sigF += fit[findex(iW,iC,FIT_F,fitorder+1,2+fissionable)]*power;
+      sigF += fit[findex(iW,iC,FIT_F,fitorder+1,FIT_F+fissionable)]*power;
   }
 
 
