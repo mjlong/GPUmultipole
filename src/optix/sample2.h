a876aba8e02013a11f69cabfeed824fc3142653d
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-10-07 11:26:57 -0400

    mv optix file to src/optix

---
+++ src/optix/sample2.h
@@ -0,0 +1,14 @@
+#ifndef __SAMPLE2_H__
+#define __SAMPLE2_H__
+#include <optix_cuda_interop.h>
+
+void createContext( int width, float R1, float Hh,unsigned int num, RTcontext* context, RTbuffer* buffer, RTbuffer*, RTbuffer*, float*, long long unsigned int);
+void createMaterial( RTcontext context, RTmaterial* material);
+void createGeometryBox( RTcontext context, RTgeometry* box, float* );
+void createGeometryCylinder( RTcontext context, RTgeometry* cylinder, float,float,float,float,float,float,float);
+void createGeometrySphere( RTcontext context, RTgeometry* sphere, float*);
+void createInstances( RTcontext context, RTmaterial material, float*, int n, int m  );
+void printUsageAndExit( const char* argv0 );
+void tracemain(int, int, int, float*, long long unsigned int);
+
+#endif
