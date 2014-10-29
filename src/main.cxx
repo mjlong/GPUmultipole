88a5021900abb0ed0959090809785e97622ea4d0
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-10-29 19:27:27 -0400

    milestone with tested but useless statements; initialize isotopes; test cudpp; optix

--- src/main.cxx
+++ src/main.cxx
@@ -10,15 +10,22 @@
 #include <neutron.h>
 #include <manmemory.h>
 #include "devicebridge.h"
+
+#include "multipole.h"
+#include "optixmain.h"
+
 void printbless();
 int main(int argc, char **argv){
+// 
 //calculation dimension
+//
   unsigned gridx, blockx, gridsize;
   gridx = atoi(argv[1]);
   blockx = atoi(argv[2]);
   gridsize = gridx*blockx;
-
-//memory allocation
+//
+//simulation memory allocation
+//
   initialize_device();
   unsigned *cnt, *blockcnt;
   CMPTYPE *hostarray, *devicearray;
@@ -26,13 +33,20 @@ int main(int argc, char **argv){
   initialize_memory(&DeviceMem, &HostMem, &devicearray, &hostarray, &cnt, &blockcnt, gridx,blockx);
   printbless();
 
+//
+//Faddeeva tables
+//
 
 //create context
   RTcontext context;
   rtContextCreate(&context);
   int id=0;
   rtContextSetDevices(context, 1, &id);
-
+  float geoPara[6] = {0.48f,0.5f,50.f,1.2f,100.f,100.f};
+  //float geoPara[6] = {0.00048f,0.0005f,0.050f,0.0012f,0.100f,0.100f};
+                      //r1,  r2,  h/2, p,   t,    H/2
+  tracemain2(context, gridsize, 2, 2, geoPara, DeviceMem.nInfo);
+  //rtContextDestroy( context );
 
 //CUDPP
 //Initialize CUDPP
@@ -68,12 +82,16 @@ int main(int argc, char **argv){
   struct multipoledata *isotopes;
   isotopes = (struct multipoledata*)malloc(sizeof(struct multipoledata)*numIso);
   isotope_read(argv[5],isotopes);
+  multipole U92238(isotopes, numIso);
+  U92238.release_pointer();
 //read materials
   struct matdata *pmat=(struct matdata*)malloc(sizeof(struct matdata));
   totIso=matread(pmat,argv[6]); 
 //move on to device settings
   anyvalue( isotopes,numIso,pmat, totIso, gridx,blockx,atoi(argv[3]),atoi(argv[4]),cnt,blockcnt, hostarray,devicearray, HostMem,DeviceMem);
 
+ cudppRadixSort(sortplan, DeviceMem.nInfo.isoenergy, DeviceMem.nInfo.id, gridsize);
+  printf("tested cudppSort\n");
   release_memory(DeviceMem, HostMem, devicearray, hostarray, cnt, blockcnt);
 
   rtContextDestroy(context); 
