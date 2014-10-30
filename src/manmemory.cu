555c8b6dcae33557a55c63ab175767fbae1c42f1
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-10-29 20:50:17 -0400

    move wtable fill and release to manmemory.

--- src/manmemory.cu
+++ src/manmemory.cu
@@ -1,10 +1,67 @@
 #include "manmemory.h"
 
+#if defined (__FOURIERW)
+#include "fourierw.h"
+__constant__ CMPTYPE a[M+1];
+__constant__ CMPTYPE b[M+1];
+#endif
+
+#if defined (__QUICKW)
+#include "QuickW.h"
+#endif
+
+#if defined (__QUICKWC) || defined(__INTERPEXP)
+__constant__ CMPTYPE2 constwtable[LENGTH*LENGTH];
+#endif
+
+
 //Simulation memory allocate and deallocate
 void initialize_device(){
   cudaSetDevice(0);
   gpuErrchk(cudaSetDeviceFlags(cudaDeviceMapHost | cudaDeviceLmemResizeToMax));
 }
+
+#if defined(__FOURIERW)
+void fill_wtables(CMPTYPE** da, CMPTYPE** db){
+  gpuErrchk(cudaMalloc((void**)da, (M+1)*sizeof(CMPTYPE))); 
+  gpuErrchk(cudaMalloc((void**)db, (M+1)*sizeof(CMPTYPE))); 
+  fill_a<<<1,M+1>>>(*da,*db); 
+  cudaMemcpyToSymbol(a, *da, M*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
+  cudaMemcpyToSymbol(b, *db, M*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
+
+}
+void release_wtables(CMPTYPE* da, CMPTYPE* db){
+  gpuErrchk(cudaFree(da));
+  gpuErrchk(cudaFree(db));
+}
+#endif
+
+#if defined(__INTERPEXP)
+void fill_wtables(CComplex<CMPTYPE>** exptable){
+  gpuErrchk(cudaMalloc((void**)exptable, LENGTH*LENGTH * 2 * sizeof(CMPTYPE)));
+  fill_exp_table<<<LENGTH,LENGTH>>>(*exptable);
+  cudaMemcpyToSymbol(constwtable, *exptable, LENGTH*LENGTH*2*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
+}
+void release_wtables(CComplex<CMPTYPE>* exptable){
+  gpuErrchk(cudaFree(exptable));
+}
+#endif
+
+#if defined(__QUICKW)
+void fill_wtables(CComplex<CMPTYPE>** wtable){
+  gpuErrchk(cudaMalloc((void**)wtable, LENGTH*LENGTH * 2 * sizeof(CMPTYPE)));
+  fill_w_tabulated<<<LENGTH,LENGTH>>>(*wtable);
+#if defined(__QUICKWC)
+  cudaMemcpyToSymbol(constwtable, *wtable, LENGTH*LENGTH*2*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
+#endif
+#if defined(__QUICKWT)
+  bindwtable(*wtable);
+#endif
+}
+void release_wtables(CComplex<CMPTYPE>* wtable){
+  gpuErrchk(cudaFree(wtable));
+}
+#endif
 
 void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, CMPTYPE** devicearray, CMPTYPE** hostarray, unsigned **cnt, unsigned** blockcnt, unsigned gridx, unsigned blockx ){
   unsigned gridsize;
--- src/manmemory.h
+++ src/manmemory.h
@@ -13,4 +13,21 @@ void initialize_memory(MemStruct* DeviceMem, MemStruct* HostMem, CMPTYPE** devic
 void release_memory(MemStruct DeviceMem, MemStruct HostMem, CMPTYPE* devicearray, CMPTYPE* hostarray, unsigned *cnt, unsigned* blockcnt );
 
 //Faddeeva function table management
+#if defined(__FOURIERW)
+void fill_wtables(CMPTYPE** da, CMPTYPE** db);
+void release_wtables(CMPTYPE* da, CMPTYPE* db);
+#endif
+
+#if defined(__INTERPEXP)
+void fill_wtables(CComplex<CMPTYPE>** exptable);
+void release_wtables(CComplex<CMPTYPE>* exptable);
+#endif
+
+#if defined(__QUICKW)
+void fill_wtables(CComplex<CMPTYPE>** wtable);
+void release_wtables(CComplex<CMPTYPE>* wtable);
+#endif
+
+
+
 #endif
