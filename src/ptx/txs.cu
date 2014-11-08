8132721a79526e3f7150f5f848f749e3bad3e749
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-11-08 01:15:04 -0500

    sigh, mit faddeeva fails probably due to array defined in Faddeeva.cu; quickwc failed because finding no way to feed constant memory

--- src/ptx/txs.cu
+++ src/ptx/txs.cu
@@ -1,6 +1,15 @@
 #include "multipole.h"
 #include "CComplex.h"
 #include <optix.h>
+
+#if defined (__QUICKWC) 
+__constant__ CMPTYPE2 constwtable[LENGTH*LENGTH];
+#endif
+
+#if defined (__MITW)
+#include "Faddeeva.h"
+#define w_function Faddeeva::w
+#endif
 rtBuffer<float, 1>              input_pos_x_buffer;
 rtBuffer<unsigned int, 1>       dev_integers;
 rtBuffer<CComplex<double>, 1>   mpdata;
@@ -13,6 +22,10 @@ using namespace optix;
 RT_CALLABLE_PROGRAM double xs_eval(double E){
   CComplex<double> z=mpdata[launch_index];
   printf("z=%g i%+g\n",real(z),imag(z));
+  //CComplex<double>z1 = CComplex<double>(constwtable[0].x,constwtable[0].y);
+  CComplex<double> z1 = w_function(z,0.00001);
+  printf("z1=%g i%+g\n",real(z1),imag(z1));
+
   return 1/E;
 }
 
