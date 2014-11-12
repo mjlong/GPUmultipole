69109484296c69e7109b91940d1a82fc4ef73286
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-11-12 15:02:09 -0500

    trivial change in tests

--- src/ptx/txs.cu
+++ src/ptx/txs.cu
@@ -1,4 +1,4 @@
-#include "multipole.h"
+//#include "multipole.h"
 #include "CComplex.h"
 #include <optix.h>
 
@@ -22,16 +22,22 @@ using namespace optix;
 
 RT_CALLABLE_PROGRAM double xs_eval(double E){
   CComplex<double> z=mpdata[launch_index];
-  printf("z=%g i%+g\n",real(z),imag(z));
+  printf("z=%g%+gi\n",real(z),imag(z));
 #if defined(__QUICKWC)
   CComplex<double>z1 = CComplex<double>(constwtable[34].x,constwtable[34].y);
   CComplex<double>z2 = wtable_buffer[34];
+  printf("constant in cu,       z1=%g%+gi\n",real(z1),imag(z1));
+  printf("from global,          z2=%g%+gi\n",real(z2),imag(z2));
 #endif
 #if defined(__MITW)
+  CComplex<double> zzzz = CComplex<double>(2.2,1.3);
+  printf("zzzz=%g%+gi\n",real(zzzz),imag(zzzz));
+
+  //printf("fakew(%g)=%g\n",3.2, fakew(3.2));
+  //double xx = fakew(2.2);
   CComplex<double> z1 = w_function(z,0.00001);
+  //printf("F(%g%+gi)=%g%+gi\n",real(z),imag(z),real(z1),imag(z1));
 #endif
-  printf("constant in cu,       z1=%g i%+g\n",real(z1),imag(z1));
-  printf("from global,          z2=%g i%+g\n",real(z2),imag(z2));
   return 1/E;
 }
 
