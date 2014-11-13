ceaeb36f21102c5654c49ccbad6bd4f6dd508fd4
Jilang Miao 缪佶朗@smithwick <jlmiao@mit.edu>
2014-11-13 18:28:02 -0500

    confirmed MIT Faddeeva, clean txs.cu for real xs eval

--- src/ptx/txs.cu
+++ src/ptx/txs.cu
@@ -27,24 +27,10 @@ rtDeclareVariable(unsigned int, launch_dim,   rtLaunchDim, );
 using namespace optix;
 
 RT_CALLABLE_PROGRAM double xs_eval(double E){
-  CComplex<double> z=mpdata[launch_index];
-  printf("z=%g%+gi\n",real(z),imag(z));
-#if defined(__QUICKWC)
-  CComplex<double>z1 = CComplex<double>(constwtable[34].x,constwtable[34].y);
-  CComplex<double>z2 = wtable_buffer[34];
-  printf("constant in cu,       z1=%g%+gi\n",real(z1),imag(z1));
-  printf("from global,          z2=%g%+gi\n",real(z2),imag(z2));
-#endif
 #if defined(__MITW)
-  CComplex<double> zzzz = CComplex<double>(2.2,1.3);
-  printf("zzzz=%g%+gi\n",real(zzzz),imag(zzzz));
-  CComplex<double> z1 = w_function(z,0.00001);
-  printf("F(%g%+gi)=%g%+gi\n",real(z),imag(z),real(z1),imag(z1));
+  
 #endif
-#if defined(__FOURIERW)
-  CComplex<double> z1 = w_function(z);
-  printf("F(%g%+gi)=%g%+gi\n",real(z),imag(z),real(z1),imag(z1));
-#endif
+
   return 1/E;
 }
 
