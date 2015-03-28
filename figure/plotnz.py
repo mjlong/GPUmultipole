4c89fe168939756a1d06ffba1dbdfa79ce257961
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-03-27 23:49:02 -0400

    finish t_nz plotter

--- figure/plotnz.py
+++ figure/plotnz.py
@@ -3,5 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-lines = np.loadtxt('t_nz',delimiter=' ',unpack=False);
-
+lines = np.loadtxt("t_nz",unpack=False);
+plt.plot(lines[:,0],lines[:,1],lines[:,0],lines[:,2]);
+plt.legend(('CPU','GPU'),loc=1,prop={'size':8})
+plt.tight_layout();
+plt.savefig('tvsnz.png')
