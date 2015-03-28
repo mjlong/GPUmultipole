54c92517b8d4e4a77e320a6994802db23a83bc56
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-03-28 01:19:50 -0400

    plotnz use Agg so that putyy can plot;

--- figure/plotnz.py
+++ figure/plotnz.py
@@ -1,9 +1,11 @@
 #~/usr/bin/python3
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-lines = np.loadtxt("t_nz",unpack=False);
+lines = np.loadtxt("../t_nz",unpack=False);
 plt.plot(lines[:,0],lines[:,1],lines[:,0],lines[:,2]);
 plt.legend(('CPU','GPU'),loc=1,prop={'size':8})
 plt.tight_layout();
