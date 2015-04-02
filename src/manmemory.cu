2b63bfdfc3859beb508cb40736b6035832fdbdfd
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-02 16:46:36 -0400

    rm wfunction table from memory

--- src/manmemory.cu
+++ src/manmemory.cu
@@ -1,20 +1,4 @@
 #include "manmemory.h"
-__constant__ float spectrumbins[NUM_BINS+1];
-
-#if defined (__FOURIERW)
-#include "fourierw.h"
-__constant__ CMPTYPE a[M+1];
-__constant__ CMPTYPE b[M+1];
-#endif
-
-#if defined (__QUICKW)
-#include "QuickW.h"
-#endif
-
-#if defined (__QUICKWC) || defined(__INTERPEXP)
-__constant__ CMPTYPE2 constwtable[LENGTH*LENGTH];
-#endif
-
 
 //Simulation memory allocate and deallocate
 void initialize_device(){
@@ -22,53 +6,6 @@ void initialize_device(){
   gpuErrchk(cudaSetDeviceFlags(cudaDeviceMapHost | cudaDeviceLmemResizeToMax));
 }
 
-#if defined(__FOURIERW)
-void fill_wtables(CMPTYPE** da, CMPTYPE** db){
-  gpuErrchk(cudaMalloc((void**)da, (M+1)*sizeof(CMPTYPE))); 
-  gpuErrchk(cudaMalloc((void**)db, (M+1)*sizeof(CMPTYPE))); 
-  fill_a<<<1,M+1>>>(*da,*db); 
-  cudaMemcpyToSymbol(a, *da, M*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
-  cudaMemcpyToSymbol(b, *db, M*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
-
-}
-void release_wtables(CMPTYPE* da, CMPTYPE* db){
-  gpuErrchk(cudaFree(da));
-  gpuErrchk(cudaFree(db));
-}
-#endif
-
-#if defined(__INTERPEXP)
-void fill_wtables(CComplex<CMPTYPE>** exptable){
-  gpuErrchk(cudaMalloc((void**)exptable, LENGTH*LENGTH * 2 * sizeof(CMPTYPE)));
-  fill_exp_table<<<LENGTH,LENGTH>>>(*exptable);
-  cudaMemcpyToSymbol(constwtable, *exptable, LENGTH*LENGTH*2*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
-}
-void release_wtables(CComplex<CMPTYPE>* exptable){
-  gpuErrchk(cudaFree(exptable));
-}
-#endif
-
-#if defined(__QUICKW)
-void fill_wtables(CComplex<CMPTYPE>** wtable){
-  gpuErrchk(cudaMalloc((void**)wtable, LENGTH*LENGTH * 2 * sizeof(CMPTYPE)));
-  fill_w_tabulated<<<LENGTH,LENGTH>>>(*wtable);
-#if defined(__QUICKWC)
-  cudaMemcpyToSymbol(constwtable, *wtable, LENGTH*LENGTH*2*sizeof(CMPTYPE), 0, cudaMemcpyDeviceToDevice);
-#endif
-#if defined(__QUICKWT)
-  bindwtable(*wtable);
-#endif
-}
-void release_wtables(CComplex<CMPTYPE>* wtable){
-  gpuErrchk(cudaFree(wtable));
-}
-#endif
-
-void assign_tallybins(float *h_tallybins, float **d_tallybins,unsigned nbinedge){
-  gpuErrchk(cudaMalloc((void**)(d_tallybins), nbinedge*sizeof(float)));
-  gpuErrchk(cudaMemcpy(*d_tallybins,h_tallybins,nbinedge*sizeof(float),cudaMemcpyHostToDevice));
-  gpuErrchk(cudaMemcpyToSymbol(spectrumbins, *d_tallybins, nbinedge*sizeof(float), 0, cudaMemcpyDeviceToDevice));
-}
 
 void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx ){
   unsigned gridsize;
