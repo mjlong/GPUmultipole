e717d0e45669881091021e7bd6458e123303269a
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-02 17:04:50 -0400

    rm wtable from manmemory

--- src/manmemory.h
+++ src/manmemory.h
@@ -3,7 +3,6 @@
 
 #include "gpuerrchk.h"
 #include "neutron.h"
-#include "CComplex.h"
 
 //initialize device
 void initialize_device();
@@ -11,23 +10,6 @@ void initialize_device();
 //Simulation memory allocate and deallocate
 void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx );
 void release_memory(MemStruct DeviceMem, MemStruct HostMem);
-
-//Faddeeva function table management
-#if defined(__FOURIERW)
-void fill_wtables(CMPTYPE** da, CMPTYPE** db);
-void release_wtables(CMPTYPE* da, CMPTYPE* db);
-#endif
-
-#if defined(__INTERPEXP)
-void fill_wtables(CComplex<CMPTYPE>** exptable);
-void release_wtables(CComplex<CMPTYPE>* exptable);
-#endif
-
-#if defined(__QUICKW)
-void fill_wtables(CComplex<CMPTYPE>** wtable);
-void release_wtables(CComplex<CMPTYPE>* wtable);
-#endif
-
 
 
 #endif
