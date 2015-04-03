85f658085d502f4331baecac98fc6aeb9e4bc062
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-03 18:17:10 -0400

    no need to count terminated neutrons, no need to use shared memory for each kernel

--- src/devicebridge.cu
+++ src/devicebridge.cu
@@ -45,7 +45,7 @@ unsigned setbank(MemStruct DeviceMem, unsigned gridsize){
 }
 
 void start_neutrons(unsigned gridx, unsigned blockx, MemStruct DeviceMem, unsigned num_src,unsigned active,unsigned banksize){
-  history<<<gridx, blockx, blockx*sizeof(unsigned)>>>(DeviceMem, num_src,active,banksize);
+  history<<<gridx, blockx/*, blockx*sizeof(unsigned)*/>>>(DeviceMem, num_src,active,banksize);
 } 
 
 unsigned count_neutrons(unsigned gridx, unsigned blockx, MemStruct DeviceMem, MemStruct HostMem, unsigned num_src){
