b9b04641de2c4a3aedf6b739420149b5dd7de877
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-03 18:16:39 -0400

    use double rand; no need to reduce terminated neutrons

--- src/simulation.cu
+++ src/simulation.cu
@@ -4,7 +4,7 @@ __global__ void initialize(MemStruct pInfo,float width){
   //int id = ((blockDim.x*blockDim.y*blockDim.z)*(blockIdx.y*gridDim.x+blockIdx.x)+(blockDim.x*blockDim.y)*threadIdx.z+blockDim.x*threadIdx.y+threadIdx.x);//THREADID;
   int id = blockDim.x * blockIdx.x + threadIdx.x;
   /* Each thread gets same seed, a different sequence number, no offset */
-  curand_init(1234, id, 0, &(pInfo.nInfo.rndState[id]));
+  curand_init(id*75684321478, id, 0, &(pInfo.nInfo.rndState[id]));
 
   neutron_sample(pInfo.nInfo, id,width);
   pInfo.nInfo.id[id] = id;
@@ -15,7 +15,7 @@ __device__ void neutron_sample(NeutronInfoStruct nInfo, unsigned id,float width)
   nInfo.live[id] = 1u;
   curandState state = nInfo.rndState[id];
   //TODO: source sampling should take settings dependent on geometry
-  nInfo.pos_x[id] = width*curand_uniform(&state);
+  nInfo.pos_x[id] = width/PI*acos(1-2*curand_uniform_double(&state));
   nInfo.pos_y[id] = 0.5f+0.00*curand_uniform(&state);
   nInfo.pos_z[id] = 0.5f+0.00*curand_uniform(&state);
   nInfo.dir_polar[id] = curand_uniform(&state)*2-1;
@@ -36,21 +36,21 @@ __global__ void history(MemStruct DeviceMem, unsigned num_src,unsigned active,un
   float Pc = Ps+DeviceMem.wdspp[4];
   float s;
   //try others when real simulation structure becomes clear
-  int idl = threadIdx.x;
+  //int idl = threadIdx.x;
   //id is the thread index
   //nid is the sampled index to get neutron position
   //in this scheme, normalization is realized by forcefully 
   //select gridsize neutrons from banksize neutrons
   int id = blockDim.x * blockIdx.x + threadIdx.x;
   curandState localState = DeviceMem.nInfo.rndState[id];
-  int nid = int(curand_uniform(&localState)*banksize);
-  extern __shared__ unsigned blockTerminated[];
+  int nid = int(curand_uniform_double(&localState)*banksize);
+  //extern __shared__ unsigned blockTerminated[];
 
   CMPTYPE rnd;
   float x = DeviceMem.nInfo.pos_x[nid];
 
 
-  int dir = 1-2*int((curand_uniform(&localState))<=0.5);
+  int dir = 1-2*int((curand_uniform_double(&localState))<=0.5);
   /* Copy state to local memory for efficiency */ 
 
   int newneu;
@@ -58,7 +58,7 @@ __global__ void history(MemStruct DeviceMem, unsigned num_src,unsigned active,un
   //printf("[%2d],x=%.5f,pf=%.5f\n",id,DeviceMem.nInfo.pos_x[nid],pf);
   //for(istep=0;istep<devstep;istep++){
   while(live){
-    s = -log(curand_uniform(&localState))*mfp;
+    s = -log(curand_uniform_double(&localState))*mfp;
     x = x+s*dir;
 
     while(!notleak(x,width)){
@@ -67,13 +67,13 @@ __global__ void history(MemStruct DeviceMem, unsigned num_src,unsigned active,un
     }
     DeviceMem.tally.cnt[int(x/dx)*gridDim.x*blockDim.x+id]+=1;
     
-    rnd = curand_uniform(&localState);
+    rnd = curand_uniform_double(&localState);
     if(rnd<Ps)
-      dir = 1-2*int((curand_uniform(&localState))<=0.5);
+      dir = 1-2*int((curand_uniform_double(&localState))<=0.5);
     else{
       live = 0;
       if(rnd>Pc){ //fission
-	rnd = curand_uniform(&localState);
+	rnd = curand_uniform_double(&localState);
 	//newneu = 2*(rnd<=0.55)+3*(rand>0.55);
 	newneu = 1-2*(rnd<=0.55); //-1 --> 2 fission; +1 --> 3 fission
 	DeviceMem.nInfo.pos_y[id] = x*newneu;
@@ -84,7 +84,7 @@ __global__ void history(MemStruct DeviceMem, unsigned num_src,unsigned active,un
     }//end collision type
   }//end one history
   //}
-  blockTerminated[idl] =1;// !live;
+  //blockTerminated[idl] =1;// !live;
   
   /*Note: from now on, live does not indicate neutron but thread active */
   //blockActive[threadIdx.x] = (((terminated*2)*blockDim.x*gridDim.x + atomicAdd(Info.num_terminated_neutrons, terminated)) < num_src);
@@ -100,7 +100,7 @@ __global__ void history(MemStruct DeviceMem, unsigned num_src,unsigned active,un
     //so, 0 instead of !live is used 
     //it was incorrect, above senario forgot to count leak neutron as terminated
   }
-  */
+
   //TODO: no need of such within block reduction for remaining()
   __syncthreads();
   live = blockDim.x>>1;
@@ -115,7 +115,7 @@ __global__ void history(MemStruct DeviceMem, unsigned num_src,unsigned active,un
     //following is to count moderation times
     DeviceMem.block_terminated_neutrons[blockIdx.x] = blockTerminated[0];
   }
-
+  */
 }
 
 __global__ void reduce_sum_plus(unsigned *threadcnt, unsigned* cnt){
