6aaaa85cf5027f9805f76372b2e98eabcb382b87
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-04 12:19:52 -0400

    plot more

--- figure/ploten.py
+++ figure/ploten.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt
-import numpy as np
 import sys
 
 num = len(sys.argv)-1;#num must be at least 2
 print num;
+dir = '../rand1/'
+colors = ('b','g','r','c','m','y');
 
 # ==============================================================================
 # ============================plot over batches ================================
 print 'plotting over batches'
 plt.figure();
 for i in range(num):
-  x=np.loadtxt('../ASE_EASE_'+sys.argv[i+1],delimiter=' ',unpack=False);
+  print 'plotting ASE of'+sys.argv[i+1]
+  x=np.loadtxt(dir+'ASE_EASE_'+sys.argv[i+1],delimiter=' ',unpack=False);
   ASE = x[1:,0];
   #EASE= x[1:,1];
   nhis = x[0,0];
   nbat = x[0,1];
+  print nhis, nbat
   #nbin = len(tally);
   space = np.linspace(1,nbat,nbat);
-  plt.loglog(space,ASE,label='Average Square Error nhis:'+str(int(nhis)));
-  print 'plotting ASE of'+sys.argv[i+1]
-
+  plt.loglog(space,ASE,color=colors[i],label='Average Square Error nhis:'+str(int(nhis)));
+  
 print 'plotting EASE from'+sys.argv[num];
-x=np.loadtxt('../ASE_EASE_'+sys.argv[num],delimiter=' ',unpack=False);
+x=np.loadtxt(dir+'ASE_EASE_'+sys.argv[num],delimiter=' ',unpack=False);
 EASE= x[1:,1];
 
 plt.loglog(space,EASE,'-',color='black',label='Expectation of ASE');
@@ -36,30 +37,31 @@ plt.savefig('ASE-n'+'.png');
 
 # ==============================================================================
 # ============================plot over histories ================================
-'''
-print 'plotting over histories'
+#'''
+print 'plotting all batches in one'
 plt.figure();
 for i in range(num):
-  x=np.loadtxt('../ASE_EASE_'+sys.argv[i+1],delimiter=' ',unpack=False);
+  x=np.loadtxt(dir+'ASE_EASE_'+sys.argv[i+1],delimiter=' ',unpack=False);
   ASE = x[1:,0];
   EASE= x[1:,1];
   nhis = x[0,0];
   nbat = x[0,1];
   #nbin = len(tally);
-  space = np.linspace(1,nbat,nbat)*nhis;
-  plt.loglog(space,ASE,label='Average Square Error nhis:'+str(int(nhis)));
-  plt.loglog(space,EASE,label='Expectation of ASE:'+str(int(nhis)));
+  space = np.linspace(1,nbat,nbat);
+  plt.loglog(space,ASE,'.',color=colors[i],label='Average Square Error nhis:'+str(int(nhis)));
+  plt.loglog(space,EASE,color=colors[i],label='Expectation of ASE:'+str(int(nhis)));
   print 'plotting ASE of'+sys.argv[i+1]
 
 plt.legend(loc='lower left',prop={'size':10});
 plt.xlabel('number of histories');
 plt.ylabel('Absolute error (/cm/neutron)^2');
-plt.savefig('ASE-h'+'.png');
-'''
+plt.savefig('ASE-n-more'+'.png');
+#'''
 
+'''
 for i in range(num):
-  print 'plotting independent'+sys.argv[i+1]
-  x=np.loadtxt('../ASE_EASE_'+sys.argv[i+1],delimiter=' ',unpack=False);
+  print 'plotting independent ASE EASE '+sys.argv[i+1]
+  x=np.loadtxt(dir+'ASE_EASE_'+sys.argv[i+1],delimiter=' ',unpack=False);
   ASE = x[1:,0];
   EASE= x[1:,1];
   nhis = x[0,0];
@@ -75,17 +77,16 @@ for i in range(num):
   plt.title( 'numhist='+str(int(nhis)))
   plt.savefig('ASE-n'+sys.argv[i+1]+'.png');
 
-
-plt.figure();
-print 'plotting distribution from'+sys.argv[num]
-x=np.loadtxt('../boxtally_'+sys.argv[num],delimiter=' ',unpack=False);
-tally = x[:,0];
-refer = x[:,1];
-plt.figure();
-plt.plot(tally);
-plt.plot(refer);
-plt.savefig('dist'+'.png');
-
+  plt.figure();
+  print 'plotting independent distribution from'+sys.argv[i+1]
+  x=np.loadtxt(dir+'boxtally_'+sys.argv[i+1],delimiter=' ',unpack=False);
+  tally = x[:,0];
+  refer = x[:,1];
+  plt.figure();
+  plt.plot(tally);
+  plt.plot(refer);
+  plt.savefig('dist'+sys.argv[i+1]+'.png');
+  
 
 x = np.loadtxt('../acc_0',delimiter=' ',unpack=False);
 rho0 = x[0];
@@ -98,3 +99,4 @@ plt.plot(space,rhos);
 plt.plot(space,rho0*(q**space));
 plt.savefig('acc.png');
 
+'''
