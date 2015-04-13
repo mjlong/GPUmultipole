dd688e7cd7c1aac27b0e1e241275603d2105fbaa
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-13 12:21:07 -0400

    forgot to add h5reader to repo

---
+++ src/h5reader.cc
@@ -0,0 +1,55 @@
+#include "stdlib.h"
+#include "hdf5.h"
+
+void readh5_(char* filename, int* cnt){
+
+  hid_t       file_id, dataset_id;  /* identifiers */
+  herr_t      status;
+
+  file_id = H5Fopen(filename, H5F_ACC_RDONLY, H5P_DEFAULT);
+  dataset_id = H5Dopen(file_id, "batch_cnt", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (int*)cnt);
+  status = H5Dclose(dataset_id);
+  status = H5Fclose(file_id);
+}
+
+
+void readh5_(char* filename, int* gridsize, int* nbat, 
+	     int* meshes, double* width, 
+	     double* sigt, double* pf, double* pc){
+
+  hid_t       file_id, dataset_id;  /* identifiers */
+  herr_t      status;
+
+  file_id = H5Fopen(filename, H5F_ACC_RDONLY, H5P_DEFAULT);
+
+  dataset_id = H5Dopen(file_id, "num_history", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (int*)gridsize);
+  status = H5Dclose(dataset_id);
+
+  dataset_id = H5Dopen(file_id, "num_batch", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (int*)nbat);
+  status = H5Dclose(dataset_id);
+
+  dataset_id = H5Dopen(file_id, "num_cells", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (int*)meshes);
+  status = H5Dclose(dataset_id);
+
+  dataset_id = H5Dopen(file_id, "width", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, (double*)width);
+  status = H5Dclose(dataset_id);
+
+  dataset_id = H5Dopen(file_id, "sigma", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, (double*)sigt);
+  status = H5Dclose(dataset_id);
+
+  dataset_id = H5Dopen(file_id, "pf", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, (double*)pf);
+  status = H5Dclose(dataset_id);
+
+  dataset_id = H5Dopen(file_id, "pc", H5P_DEFAULT);
+  status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, (double*)pc);
+  status = H5Dclose(dataset_id);
+
+  status = H5Fclose(file_id);
+}
