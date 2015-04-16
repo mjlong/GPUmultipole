7037afc565c095466dcd126420bb1a34af92dc20
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-16 17:51:24 -0400

    make h5writer create group and take group name

--- src/h5writer.cc
+++ src/h5writer.cc
@@ -11,15 +11,17 @@ long vector with size length X num_vec;
 allocating "num_vec" vectors with length length will NOT work
  */
 void createmptyh5(char *filename){
-  hid_t file /*,group*/;
+  hid_t file, group;
   herr_t status;
-  file = H5Fcreate (filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
-  //group = H5Gcreate (file, "", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-  //status = H5Gclose (group);
-  status = H5Fclose (file);
+  file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+  group =H5Gcreate(file, "/scatter", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  status = H5Gclose(group);
+  group =H5Gcreate(file, "/tally", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  status = H5Gclose(group);
+  status = H5Fclose(file);
 }
 
-void writeh5_nxm_(char *filename, char *dsetname, int *vec1, int *num_vec, int *length){
+void writeh5_nxm_(char *filename, char* groupname, char *dsetname, int *vec1, int *num_vec, int *length){
 
   hid_t       file,group, dataset;         /* file and dataset handles */
   hid_t       datatype, dataspace;   /* handles */
@@ -36,7 +38,7 @@ void writeh5_nxm_(char *filename, char *dsetname, int *vec1, int *num_vec, int *
 
 
   /* Open an existing group of the specified file. */
-  group = H5Gopen(file, "/", H5P_DEFAULT);
+  group = H5Gopen(file, groupname, H5P_DEFAULT);
 
   /*
    * Describe the size of the array and create the data space for fixed
@@ -77,7 +79,7 @@ void writeh5_nxm_(char *filename, char *dsetname, int *vec1, int *num_vec, int *
 }
 
 
-void writeh5_nxm_(char *filename, char *dsetname, double *vec1, int *num_vec, int *length){
+void writeh5_nxm_(char *filename, char *groupname, char *dsetname, double *vec1, int *num_vec, int *length){
 
   hid_t       file,group, dataset;         /* file and dataset handles */
   hid_t       datatype, dataspace;   /* handles */
@@ -94,7 +96,7 @@ void writeh5_nxm_(char *filename, char *dsetname, double *vec1, int *num_vec, in
 
 
   /* Open an existing group of the specified file. */
-  group = H5Gopen(file, "/", H5P_DEFAULT);
+  group = H5Gopen(file, groupname, H5P_DEFAULT);
 
   /*
    * Describe the size of the array and create the data space for fixed
