9b7ed01d39b4dfd2d4042b62590cc5bb44991387
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-16 16:44:13 -0400

    put 1d kernel and 3d kernels into preprocess macros; only do tally when __TALLY is defined

--- src/simulation.h
+++ src/simulation.h
@@ -10,13 +10,16 @@
 #define ENDENERG 0.00001
 #define MAXENERGY 30000.0
 __device__ void neutron_sample(NeutronInfoStruct nInfo, unsigned id,float width);
-__global__ void history(MemStruct, unsigned num_src,unsigned active,unsigned banksize);
 __global__ void initialize(MemStruct,float,int);
 __global__ void reduce_sum_plus(int *threadcnt, int* cnt);
 __global__ void reduce_sum_equal(int* thread_active, int* active);
 __device__ unsigned notleak(float x,float a);
+#if defined(__1D)
+__global__ void history(MemStruct, unsigned num_src,unsigned active,unsigned banksize);
 __global__ void history_ref(MemStruct DeviceMem, unsigned num_src,unsigned active,unsigned banksize);
+#endif
+#if defined(__3D)
 __global__ void history_3d_ref(MemStruct DeviceMem, unsigned num_src,unsigned active,unsigned banksize);
-
+#endif
 #define TEPSILON 1.0e-5
 #endif
