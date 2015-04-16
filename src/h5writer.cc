994c5558f9803635eea0081390b687347eec4501
Jilang Miao 缪佶朗@wills-workstation <jlmiao@mit.edu>
2015-04-16 18:13:26 -0400

    add function to write float array

--- src/h5writer.cc
+++ src/h5writer.cc
@@ -64,7 +64,7 @@ void writeh5_nxm_(char *filename, char* groupname, char *dsetname, int *vec1, in
   /*
    * Write the data to the dataset using default transfer properties.
    */
-  status = H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (double*)vec1);
+  status = H5Dwrite(dataset, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (int*)vec1);
 
   /*
    * Close/release resources.
@@ -78,6 +78,63 @@ void writeh5_nxm_(char *filename, char* groupname, char *dsetname, int *vec1, in
   return;
 }
 
+
+void writeh5_nxm_(char *filename, char* groupname, char *dsetname, float *vec1, int *num_vec, int *length){
+
+  hid_t       file,group, dataset;         /* file and dataset handles */
+  hid_t       datatype, dataspace;   /* handles */
+  hsize_t     dimsf[2];              /* dataset dimensions */
+  herr_t      status;                             
+
+  /*
+   * Create a new file using H5F_ACC_TRUNC access,
+   * default file creation properties, and default file
+   * access properties.
+   */
+  //file = H5Fcreate(filename, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+  file = H5Fopen(filename, H5F_ACC_RDWR, H5P_DEFAULT);
+
+
+  /* Open an existing group of the specified file. */
+  group = H5Gopen(file, groupname, H5P_DEFAULT);
+
+  /*
+   * Describe the size of the array and create the data space for fixed
+   * size dataset. 
+   */
+  dimsf[0] = *num_vec;
+  dimsf[1] = *length;
+  dataspace = H5Screate_simple(2, dimsf, NULL); 
+
+  /* 
+   * Define datatype for the data in the file.
+   * We will store little endian INT numbers.
+   */
+  datatype = H5Tcopy(H5T_NATIVE_FLOAT);
+  status = H5Tset_order(datatype, H5T_ORDER_LE);
+
+  /*
+   * Create a new dataset within the file using defined dataspace and
+   * datatype and default dataset creation properties.
+   */
+  dataset = H5Dcreate(group, dsetname, datatype, dataspace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+  /*
+   * Write the data to the dataset using default transfer properties.
+   */
+  status = H5Dwrite(dataset, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, (float*)vec1);
+
+  /*
+   * Close/release resources.
+   */
+  H5Sclose(dataspace);
+  H5Tclose(datatype);
+  H5Dclose(dataset);
+  H5Gclose(group);
+  H5Fclose(file);
+
+  return;
+}
 
 void writeh5_nxm_(char *filename, char *groupname, char *dsetname, double *vec1, int *num_vec, int *length){
 
