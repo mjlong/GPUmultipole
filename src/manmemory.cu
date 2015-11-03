f064930b016b0bfc558540e560eece71ac43ec62
jlmiao@nsecluster <jlmiao@mit.edu>
2015-11-03 16:39:06 -0500

    memory allocate and release of the bank struct

--- src/manmemory.cu
+++ src/manmemory.cu
@@ -12,22 +12,174 @@ void copymeans(int *h_cnt, int *batcnt, unsigned meshes, unsigned offset){
 
 }
 
+
+void initialize_memory_data(MemStruct *DeviceMem, MemStruct *HostMem){
+  (*HostMem).wdspp = (float*)malloc(sizeof(float)*9);
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).wdspp), 9*sizeof(float)));
+}
 void copydata(MemStruct DeviceMem, MemStruct HostMem){
   gpuErrchk(cudaMemcpy(DeviceMem.wdspp,  HostMem.wdspp,   sizeof(float)*9, cudaMemcpyHostToDevice));
   gpuErrchk(cudaMemcpyToSymbol(wdspp, DeviceMem.wdspp, 9*sizeof(float), 0, cudaMemcpyDeviceToDevice));
 }
-void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx,unsigned nbat,unsigned ubat){
+void release_memory_data(MemStruct DeviceMem, MemStruct HostMem){
+  free(HostMem.wdspp);
+  gpuErrchk(cudaFree(DeviceMem.wdspp));
+}
+//==============================================================================
+//=====memory_converge() allocates memory for the phase of source convergence===
+//1. memory_converge() takes num_seg_XL as parameter, which varies with phases==
+//2. memory_converge() need not have tally arrays, which exist current for debug
+//==============================================================================
+void allocate_memory_converge(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx,unsigned num_seg){
   unsigned gridsize,banksize;
   gridsize = gridx*blockx;
-  banksize = gridx*blockx*ubat;
-  //for __TALLY, ubat is used as tranfac
+  banksize = gridx*blockx*num_seg;
 
 #if defined(__TALLY)
-#if defined(__MTALLY) //Fission Matrix Tally
+#if defined(__FTALLY)||(__FTALLY2) //Fission source tally
   gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.imat),  banksize*3*sizeof(int)));
-  (*HostMem).batcnt     = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins*numbins);
-  memset((*HostMem).batcnt, 0, sizeof(CMPTYPE)*numbins*numbins);
+  (*HostMem).nInfo.live  = (int*)malloc(sizeof(int)*banksize);
+  (*HostMem).batcnt     = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
+  memset((*HostMem).batcnt, 0, sizeof(CMPTYPE)*numbins);
 #endif
+#if defined(__CTALLY) //Collision density (flux) tally
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).spectrum), numbins*sizeof(CMPTYPE)));
+  (*HostMem).spectrum = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);  
+
+  (*HostMem).batcnt     = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).batcnt), numbins*sizeof(CMPTYPE)));
+#if defined(__CTALLY2)
+  (*HostMem).batcnt2    = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).batcnt2),numbins*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).cnt2_t),numbins*gridsize*sizeof(int)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).tally.cnt2), gridsize*numbins*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMemset((*DeviceMem).tally.cnt2,0, numbins*gridsize*sizeof(CMPTYPE)));  
+#endif
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).block_spectrum), numbins*gridx*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMemset((*DeviceMem).block_spectrum, 0, numbins*gridx*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).tally.cnt), gridsize*numbins*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMemset((*DeviceMem).tally.cnt, 0, numbins*gridsize*sizeof(CMPTYPE)));  
+#endif
+
+#endif
+
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.live),       banksize*sizeof(int)));
+  gpuErrchk(cudaMemset((*DeviceMem).nInfo.live, 0, banksize*sizeof(int)));  
+
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.rndState), gridsize*sizeof(curandState)));
+
+#if defined(__1D)
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_x),3*banksize*sizeof(float)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_y),banksize*sizeof(float)));
+#endif 
+
+#if defined(__3D)
+  (*HostMem).nInfo.pos_x = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).nInfo.pos_y = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).nInfo.pos_z = (float*)malloc(sizeof(float)*banksize);
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_x),3*banksize*sizeof(float)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_y),3*banksize*sizeof(float)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_z),3*banksize*sizeof(float)));
+#endif
+
+  return;
+}
+
+
+void release_memory_converge(MemStruct DeviceMem, MemStruct HostMem){
+#if defined(__TALLY)
+#if defined(__FTALLY)||(__FTALLY2) //Fission source tally
+  gpuErrchk(cudaFree(DeviceMem.nInfo.imat));
+  free(HostMem.nInfo.live);
+  free(HostMem.batcnt);
+#endif
+#if defined(__CTALLY) //Collision density (flux) tally
+  gpuErrchk(cudaFree(DeviceMem.spectrum));
+  free(HostMem.spectrum);
+  free(HostMem.spectrum);  
+
+  free(HostMem.batcnt);
+  gpuErrchk(cudaFree(DeviceMem.batcnt));
+#if defined(__CTALLY2)
+  free(HostMem.batcnt2);
+  gpuErrchk(cudaFree(DeviceMem.batcnt2));
+  gpuErrchk(cudaFree(DeviceMem.cnt2_t));
+  gpuErrchk(cudaFree(DeviceMem.tally.cnt2));
+#endif
+  gpuErrchk(cudaFree(DeviceMem.block_spectrum));
+  gpuErrchk(cudaFree(DeviceMem.tally.cnt));
+#endif
+#endif
+  gpuErrchk(cudaFree(DeviceMem.nInfo.live));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.rndState));
+
+#if defined(__1D)
+  gpuErrchk(cudaFree(DeviceMem).nInfo.pos_x),3*banksize*sizeof(float)));
+  gpuErrchk(cudaFree(DeviceMem).nInfo.pos_y),banksize*sizeof(float)));
+#endif 
+
+#if defined(__3D)
+  free(HostMem.nInfo.pos_x);
+  free(HostMem.nInfo.pos_y);
+  free(HostMem.nInfo.pos_z);
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_x));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_y));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_z));
+#endif
+  return;
+}
+
+
+//==============================================================================
+//====================initialize memory for  delayed bank ======================
+//==============================================================================
+void initialize_memory_bank(MemStruct *HostMem, unsigned banksize){
+  (*HostMem).bank.x = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).bank.y = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).bank.z = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).bank.generation_of_birth = (int*)malloc(sizeof(int)*banksize);
+  (*HostMem).bank.time_of_use = (int*)malloc(sizeof(int)*banksize);
+  memset((*HostMem).bank.time_of_use, 0xff, sizeof(int)*banksize);
+
+  (*HostMem).bank.size   = (unsigned*)malloc(sizeof(unsigned));
+  (*HostMem).bank.cursor_start     = (unsigned*)malloc(sizeof(unsigned));
+  (*HostMem).bank.cursor_end       = (unsigned*)malloc(sizeof(unsigned));
+  (*HostMem).bank.cursor_reuse     = (unsigned*)malloc(sizeof(unsigned));
+  (*HostMem).bank.cursor_available = (unsigned*)malloc(sizeof(unsigned));
+  (*HostMem).bank.cursor_safe      = (unsigned*)malloc(sizeof(unsigned));
+  (*HostMem).bank.delta_safe       = (unsigned*)malloc(sizeof(unsigned));
+  ((*HostMem).bank.size)[0]    = banksize;
+  ((*HostMem).bank.cursor_end)[0] = 0; 
+}
+
+void release_memory_bank(MemStruct HostMem){
+  free(HostMem.bank.x);
+  free(HostMem.bank.y);
+  free(HostMem.bank.z);
+  free(HostMem.bank.generation_of_birth);
+  free(HostMem.bank.time_of_use);
+
+  free(HostMem.bank.size);
+  free(HostMem.bank.cursor_start);
+  free(HostMem.bank.cursor_end);
+  free(HostMem.bank.cursor_reuse);
+  free(HostMem.bank.cursor_available);
+  free(HostMem.bank.delta_safe);
+  free(HostMem.bank.cursor_safe);
+}
+
+
+//==============================================================================
+//=====memory_active() allocates memory for the phase of source convergence===
+//1. memory_active() takes num_seg as parameter, which varies with phases ====
+//2. memory_active() have tally array
+//==============================================================================
+void allocate_memory_active(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx,unsigned num_seg){
+  unsigned gridsize,banksize;
+  gridsize = gridx*blockx;
+  banksize = gridx*blockx*num_seg;
+
+#if defined(__TALLY)
 #if defined(__FTALLY)||(__FTALLY2) //Fission source tally
 #if defined(__FTALLY)
   gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.imat),  banksize*3*sizeof(int)));
@@ -35,6 +187,118 @@ void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbin
 #else //(__FTALLY2)
   gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.imat),  gridsize*3*sizeof(int)));
   (*HostMem).nInfo.live  = (int*)malloc(sizeof(int)*gridsize);
+#endif
+  (*HostMem).batcnt     = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
+  memset((*HostMem).batcnt, 0, sizeof(CMPTYPE)*numbins);
+#endif
+#if defined(__CTALLY) //Collision density (flux) tally
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).spectrum), numbins*sizeof(CMPTYPE)));
+  (*HostMem).spectrum = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);  
+
+  (*HostMem).batcnt     = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).batcnt), numbins*sizeof(CMPTYPE)));
+#if defined(__CTALLY2)
+  (*HostMem).batcnt2    = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).batcnt2),numbins*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).cnt2_t),numbins*gridsize*sizeof(int)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).tally.cnt2), gridsize*numbins*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMemset((*DeviceMem).tally.cnt2,0, numbins*gridsize*sizeof(CMPTYPE)));  
+#endif
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).block_spectrum), numbins*gridx*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMemset((*DeviceMem).block_spectrum, 0, numbins*gridx*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).tally.cnt), gridsize*numbins*sizeof(CMPTYPE)));
+  gpuErrchk(cudaMemset((*DeviceMem).tally.cnt, 0, numbins*gridsize*sizeof(CMPTYPE)));  
+#endif
+#endif
+
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.live),       banksize*sizeof(int)));
+  gpuErrchk(cudaMemset((*DeviceMem).nInfo.live, 0, banksize*sizeof(int)));  
+
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.rndState), gridsize*sizeof(curandState)));
+
+#if defined(__1D)
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_x),3*banksize*sizeof(float)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_y),banksize*sizeof(float)));
+#endif 
+
+#if defined(__3D)
+#if defined(__FTALLY2)
+  (*HostMem).nInfo.pos_x = (float*)malloc(sizeof(float)*gridsize);
+  (*HostMem).nInfo.pos_y = (float*)malloc(sizeof(float)*gridsize);
+  (*HostMem).nInfo.pos_z = (float*)malloc(sizeof(float)*gridsize);
+#else
+  (*HostMem).nInfo.pos_x = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).nInfo.pos_y = (float*)malloc(sizeof(float)*banksize);
+  (*HostMem).nInfo.pos_z = (float*)malloc(sizeof(float)*banksize);
+#endif
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_x),3*banksize*sizeof(float)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_y),3*banksize*sizeof(float)));
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.pos_z),3*banksize*sizeof(float)));
+#endif
+
+  return;
+}
+
+
+void release_memory_active(MemStruct DeviceMem, MemStruct HostMem){
+#if defined(__TALLY)
+#if defined(__FTALLY)||(__FTALLY2) //Fission source tally
+  gpuErrchk(cudaFree(DeviceMem.nInfo.imat));
+  free(HostMem.nInfo.live);
+  free(HostMem.batcnt);
+#endif
+#if defined(__CTALLY) //Collision density (flux) tally
+  gpuErrchk(cudaFree(DeviceMem.spectrum));
+  free(HostMem.spectrum);  
+
+  free(HostMem.batcnt);
+  gpuErrchk(cudaFree(DeviceMem.batcnt));
+#if defined(__CTALLY2)
+  free(HostMem.batcnt2);
+  gpuErrchk(cudaFree(DeviceMem.batcnt2));
+  gpuErrchk(cudaFree(DeviceMem.cnt2_t));
+  gpuErrchk(cudaFree(DeviceMem.tally.cnt2));
+#endif
+  gpuErrchk(cudaFree(DeviceMem.block_spectrum));
+  gpuErrchk(cudaFree(DeviceMem.tally.cnt));
+#endif
+#endif
+
+  gpuErrchk(cudaFree(DeviceMem.nInfo.live));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.rndState));
+
+#if defined(__1D)
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_x));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_y));
+#endif 
+
+#if defined(__3D)
+  free(HostMem.nInfo.pos_x);
+  free(HostMem.nInfo.pos_y);
+  free(HostMem.nInfo.pos_z);
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_x));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_y));
+  gpuErrchk(cudaFree(DeviceMem.nInfo.pos_z));
+#endif
+
+  return;
+}
+
+void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx,unsigned nbat,unsigned ubat){
+  unsigned gridsize,banksize;
+  gridsize = gridx*blockx;
+  banksize = gridx*blockx*ubat;
+  //for __TALLY, ubat is used as tranfac
+
+#if defined(__TALLY)
+#if defined(__FTALLY)||(__FTALLY2) //Fission source tally
+#if defined(__FTALLY)
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.imat),  banksize*3*sizeof(int)));
+  (*HostMem).nInfo.live  = (int*)malloc(sizeof(int)*banksize);
+#else //(__FTALLY2)
+  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.imat),  gridsize*3*sizeof(int)));
+  (*HostMem).nInfo.live  = (int*)malloc(sizeof(int)*gridsize);
+  printf("live allocated:%d\n",gridsize);
 #endif
   (*HostMem).batcnt     = (CMPTYPE*)malloc(sizeof(CMPTYPE)*numbins);
   memset((*HostMem).batcnt, 0, sizeof(CMPTYPE)*numbins);
@@ -84,11 +348,6 @@ void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbin
   gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.sigA),   banksize*sizeof(CMPTYPE)));
   gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.sigF),   banksize*sizeof(CMPTYPE)));
   gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.isoenergy),banksize*sizeof(CMPTYPE)));
-#endif
-
-#if defined(__SCATTERPLOT)
-  (*HostMem).nInfo.energy  = (CMPTYPE*)malloc(sizeof(CMPTYPE)*banksize);
-  gpuErrchk(cudaMalloc((void**)&((*DeviceMem).nInfo.energy),   banksize*sizeof(CMPTYPE))); //use as initial z position for plot
 #endif
 
 #if defined(__1D)
@@ -155,9 +414,6 @@ void release_memory(MemStruct DeviceMem, MemStruct HostMem){
 #endif
 #endif
   free(HostMem.nInfo.live);
-  free(HostMem.wdspp);
-
-  gpuErrchk(cudaFree(DeviceMem.wdspp));
 
   gpuErrchk(cudaFree(DeviceMem.nInfo.live));
   gpuErrchk(cudaFree(DeviceMem.nInfo.rndState));
@@ -176,10 +432,6 @@ void release_memory(MemStruct DeviceMem, MemStruct HostMem){
   gpuErrchk(cudaFree(DeviceMem.nInfo.pos_y));
 #endif
 
-#if defined(__SCATTERPLOT)
-  gpuErrchk(cudaFree(DeviceMem.nInfo.energy));
-  free(HostMem.nInfo.energy);
-#endif
 #if defined(__3D)
   free(HostMem.nInfo.pos_x);
   free(HostMem.nInfo.pos_y);
--- src/manmemory.h
+++ src/manmemory.h
@@ -7,11 +7,20 @@
 //initialize device
 void initialize_device();
 
+void initialize_memory_bank(MemStruct *HostMem, unsigned banksize);
+void release_memory_bank(MemStruct HostMem);
 //Simulation memory allocate and deallocate
+void initialize_memory_data(MemStruct *DeviceMem, MemStruct *HostMem);
+void copydata(MemStruct DeviceMem, MemStruct HostMem);
+void release_memory_data(MemStruct DeviceMem, MemStruct HostMem);
+void allocate_memory_converge(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx,unsigned num_seg);
+void   allocate_memory_active(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx,unsigned num_seg);
+void release_memory_converge(MemStruct DeviceMem, MemStruct HostMem);
+void   release_memory_active(MemStruct DeviceMem, MemStruct HostMem);
+
 void initialize_memory(MemStruct *DeviceMem, MemStruct *HostMem, unsigned numbins, unsigned gridx, unsigned blockx, unsigned numbatches,unsigned ubat);
 void release_memory(MemStruct DeviceMem, MemStruct HostMem);
 void copymeans(int *h_cnt, int *batcnt, unsigned meshes, unsigned offset);
-void copydata(MemStruct DeviceMem, MemStruct HostMem);
 void resettally(CMPTYPE *cnt, unsigned totbins);
 void resettally(int *cnt, unsigned totbins);
 #endif
